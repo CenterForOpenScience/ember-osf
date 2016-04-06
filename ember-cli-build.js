9af0cbc92e07750d45eea9edc87dae7427d81645
Sam Chrisinger <s.chrisinger@gmail.com>
2016-04-06 14:48:26 -0400

    Some cleanup of example app
    - add bootstrap
    - add link to Nodes page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,16 +3,28 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    // Add options here
-  });
+    var app = new EmberAddon(defaults, {
+    });
 
-  /*
-    This build file specifies the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
 
-  return app.toTree();
+    app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+    app.import('bower_components/bootstrap/dist/css/bootstrap-theme.min.css');
+    app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
+        destDir: 'fonts'
+    });
+    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
+        destDir: 'fonts'
+    });
+
+    return app.toTree();
 };
