a14571f210e5f180629a8d0ef6269fea9e729553
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-03 15:39:29 -0400

    Less ember-try scenarios; use ember 2.4.3

--- config/ember-try.js
+++ config/ember-try.js
@@ -2,53 +2,11 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
+      name: 'lts',
       bower: {
-        dependencies: { }
-      }
-    },
-    {
-      name: 'ember-1.13',
-      bower: {
-        dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
-        }
-      }
-    },
-    {
-      name: 'ember-release',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#release'
-        },
-        resolutions: {
-          'ember': 'release'
-        }
-      }
-    },
-    {
-      name: 'ember-beta',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#beta'
-        },
-        resolutions: {
-          'ember': 'beta'
-        }
-      }
-    },
-    {
-      name: 'ember-canary',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#canary'
-        },
-        resolutions: {
-          'ember': 'canary'
-        }
+          dependencies: {
+	      ember: '2.4.3'
+	  }
       }
     }
   ]
