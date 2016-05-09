996f4fcae7f1e22b4ea25380b1c34a9b71984a01
Henrique Moco <hmoco94@vt.edu>
2016-05-09 15:29:06 -0400

    clean ip

--- addon/models/node.js
+++ addon/models/node.js
@@ -37,7 +37,6 @@ export default OsfModel.extend({
     //forkedFrom: DS.belongsTo('node'),
     //nodeLinks:  DS.hasMany('node-pointers'),
     //registrations: DS.hasMany('registrations'),
-    //primaryInstitution: DS.belongsTo('institution'),
     root: DS.belongsTo('node')
     //logs: DS.hasMany('node-logs'),
 });
