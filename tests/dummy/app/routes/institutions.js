69fbfe6e1a2b04f9302748cc1e53b18296ecaf29
Henrique Moco <hmoco94@vt.edu>
2016-05-09 14:36:04 -0400

    point to right endpoint

--- tests/dummy/app/routes/institutions.js
+++ tests/dummy/app/routes/institutions.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('institutions');
+    return this.store.findAll('institution');
   }
 });
