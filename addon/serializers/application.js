ca7bef22cba74762c0b1691ec564201f059e8020
Andy Boughton <abought@gmail.com>
2016-05-10 11:56:29 -0400

    Add wip code to create/edit (test server writes)

--- addon/serializers/application.js
+++ addon/serializers/application.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
     extractAttributes(modelClass, resourceHash) {
-        //ApiV2 `links` exist outside the attributes field; make them accessible to the data model
+        // ApiV2 `links` exist outside the attributes field; make them accessible to the data model
         if (resourceHash.links) {  // TODO: Should also test whether model class defines a links field
             resourceHash.attributes.links = resourceHash.links;
         }
@@ -17,4 +17,12 @@ export default DS.JSONAPISerializer.extend({
             return Ember.String.camelize(key);
         }
     }
+    
+    
+    // TODO: Sending back to server requires
+    // 1. Remove links from payload (try something like serializer attrs: field{serialize:false}), and what happens if record has no links field?
+    // 2. Rename keys in payload (try setting something with snake case)
+    // 3. Veirfy post, put, and patch operations
+    // 4. Don't send relationships, if that breaks writing operations
+    
 });
