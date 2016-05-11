a7b72686c41e911f32d38db374bac90d3e05b5b3
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-11 18:27:50 -0400

    Let Registration model subclass Node

--- addon/models/registration.js
+++ addon/models/registration.js
@@ -1,22 +1,10 @@
 import DS from 'ember-data';
 
-import OsfModel from 'ember-osf/models/base';
+import Node from 'ember-osf/models/node';
 
-export default OsfModel.extend({
-    title: DS.attr('string'),
-    registration: DS.attr('boolean'),
-    public: DS.attr('boolean'),
-    category: DS.attr('string'),
-    fork: DS.attr('boolean'),
-    description: DS.attr('string'),
-    collection: DS.attr('boolean'),
-
-    dateCreated: DS.attr('date'),
-    dateModified: DS.attr('date'),
+export default Node.extend({
     dateRegistered: DS.attr('date'),
-
     pendingRegistrationApproval: DS.attr('boolean'),
-
     embargoEndDate: DS.attr('date'),
     pendingEmbargoApproval: DS.attr('boolean'),
     withdrawn: DS.attr('boolean'),
@@ -25,17 +13,14 @@ export default OsfModel.extend({
 
     registrationSupplement: DS.attr('string'),
     registeredMeta: DS.attr(),
-    tags: DS.attr(),
 
-    affiliatedInstitutions: DS.hasMany('institutions', {
-        inverse: 'registrations'
-    }),
     registeredFrom: DS.belongsTo('node', {
         inverse: 'registrations'
     }),
-    registeredBy: DS.belongsTo('user'),
+    registeredBy: DS.belongsTo('user', {
+        inverse: null
+    }),
     contributors: DS.hasMany('contributors'),
     comments: DS.hasMany('comments')
     //more relationship
-
 });
