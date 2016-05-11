6076893c52a5245d91776a4db479508f63adee1f
Chris Seto <chriskseto@gmail.com>
2016-05-11 11:03:43 -0400

    Move all embedded resources into includes

--- addon/serializers/application.js
+++ addon/serializers/application.js
@@ -2,11 +2,37 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-    
+
     // TODO: Pre-1.0, refactor this into a separate OSF serializer, so we can support other microservices such as WB
     attrs: {
         links: {serialize: false},
         embeds: {serialize: false}
+    },
+
+    _extractEmbeds(resourceHash) {
+        if (!resourceHash.embeds) {
+            return []; // Nothing to do
+        }
+        let included = [];
+        resourceHash.relationships = resourceHash.relationships || {};
+        for (let embedded in resourceHash.embeds) {
+            if (!(embedded || resourceHash.embeds[embedded])) {
+                continue;
+            }
+            //TODO Pagination probably breaks here
+            let data = resourceHash.embeds[embedded].data;
+            if (Array.isArray(data)) {
+                included = included.concat(data);
+            } else {
+                included.push(data);
+            }
+            resourceHash.embeds[embedded].type = embedded;
+            //Only needs to contain id and type but this way we don't have to special case arrays
+            resourceHash.relationships[embedded] = resourceHash.embeds[embedded];
+        }
+        delete resourceHash.embeds;
+        //Recurse in, includeds are only processed on the top level. Emebeds are nested.
+        return included.concat(included.reduce((acc, include) =>  acc.concat(this._extractEmbeds(include)), []));
     },
 
     _mergeFields(resourceHash) {
@@ -14,17 +40,15 @@ export default DS.JSONAPISerializer.extend({
         if (resourceHash.links) {  // TODO: Should also test whether model class defines a links field
             resourceHash.attributes.links = resourceHash.links;
         }
-    	if (resourceHash.embeds) {
-                resourceHash.relationships = resourceHash.relationships || {};
-                Object.keys(resourceHash.embeds).forEach(embedded => {
-                resourceHash.relationships[embedded] = resourceHash.relationships[embedded] || {};
-                resourceHash.embeds[embedded].type = embedded;
-                resourceHash.relationships[embedded] = resourceHash.embeds[embedded];
-	    });
-	}
+
         return resourceHash;
     },
-    
+
+    _normalizeDocumentHelper(documentHash) {
+        documentHash.included = this._extractEmbeds(documentHash.data);
+        return this._super(documentHash);
+    },
+
     extractAttributes(modelClass, resourceHash) {
         resourceHash = this._mergeFields(resourceHash);
         return this._super(modelClass, resourceHash);
@@ -36,7 +60,7 @@ export default DS.JSONAPISerializer.extend({
         } else if (method === 'serialize') {
             return Ember.String.camelize(key);
         }
-	return key;
+        return key;
     },
 
     serialize: function(snapshot, options) {
