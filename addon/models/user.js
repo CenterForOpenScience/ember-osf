ddaf37266463eefc2c9efa9f7129c90ae3a55b0e
Saman Ehsan <se8ea@virginia.edu>
2016-05-12 09:53:18 -0400

    CamelCase user model fields

--- addon/models/user.js
+++ addon/models/user.js
@@ -3,10 +3,10 @@ import DS from 'ember-data';
 import OsfModel from 'ember-osf/models/base';
 
 export default OsfModel.extend({
-    full_name: DS.attr('string'),
-    given_name: DS.attr('string'),
-    middle_names: DS.attr(),
-    family_name: DS.attr('string'),
+    fullName: DS.attr('string'),
+    givenName: DS.attr('string'),
+    middleNames: DS.attr(),
+    familyName: DS.attr('string'),
 
     nodes: DS.hasMany('nodes')
 });
