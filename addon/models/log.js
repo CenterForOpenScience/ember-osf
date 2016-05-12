5b49e4da40ae64f2e11c34b754ed792d09aa6173
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-12 11:44:28 -0400

    Add first attempt at log model.

---
+++ addon/models/log.js
@@ -0,0 +1,18 @@
+import DS from 'ember-data';
+import OsfModel from 'ember-osf/models/base';
+
+export default OsfModel.extend({
+    date: DS.attr('date'),
+    action: DS.attr('string'),
+    params: DS.attr(),
+
+    node: DS.belongsTo('node', {
+      inverse: null
+    }),
+    original_node: DS.belongsTo('node', {inverse: 'logs'}),
+    user: DS.belongsTo('user'),
+    linked_node: DS.belongsTo('node'),
+    template_node: DS.belongsTo('node', {
+      inverse: 'templateFrom'
+    })
+});
