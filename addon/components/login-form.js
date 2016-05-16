4bcc5810fa748bd089210f939fb795c44ea2b7f5
Andy Boughton <abought@gmail.com>
2016-05-16 15:02:40 -0400

    Finish boilerplate

--- addon/components/login-form.js
+++ addon/components/login-form.js
@@ -5,7 +5,10 @@ import layout from '../templates/components/login-form';
 // TODO: Future reading- add responsiveness on delays http://blog.trackets.com/2013/05/23/how-to-write-a-login-form.html
 export default Ember.Component.extend({
     layout,
-    
+
+    identification: undefined,
+    password: undefined,
+
     actions: {
         loginSubmit() {
             this.sendAction('loginSubmit', ...arguments);
