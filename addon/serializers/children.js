73f7573adcb1862d551a50bd9f0b3cbeb8dec9fc
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-16 14:49:32 -0400

    Override payloadKeyFromModelName hook instead of serialize hook on children serializer.

--- addon/serializers/children.js
+++ addon/serializers/children.js
@@ -1,10 +1,8 @@
 import OsfSerializer from './osf-serializer';
 
 export default OsfSerializer.extend({
-    serialize: function(snapshot, options) {
-        var serialized = this._super(snapshot, options);
-        // Change type from 'children' to 'nodes'.  APIv2 does not have children type.
-        serialized.data.type = 'nodes';
-        return serialized;
+    payloadKeyFromModelName: function() {
+        // Overrides type 'children' with type 'nodes'. APIv2 expecting type 'nodes'.
+        return 'nodes';
     }
 });
