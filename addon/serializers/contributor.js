6a0bfdaa9a5f0fc72392bb2a29fa2a906f76d31b
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-16 17:39:05 -0400

    Modify contributor serializer so user id is sent under "relationships".

--- addon/serializers/contributor.js
+++ addon/serializers/contributor.js
@@ -1,4 +1,17 @@
 import OsfSerializer from './osf-serializer';
 
 export default OsfSerializer.extend({
+    serialize: function(snapshot, options) {
+        // Restore relationships to serialized data
+        var serialized = this._super(snapshot, options);
+        serialized.data.relationships = {
+            users: {
+                data: {
+                    id: serialized.data.id,
+                    type: 'users'
+                }
+            }
+        };
+        return serialized;
+    },
 });
