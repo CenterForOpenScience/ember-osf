5c8cb6c5dc56bfbb47993b0669a7a303b7da6289
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-16 17:38:34 -0400

    Modify contributor adapter to change url from /contributors/ to nodes/node_id/contributors.

--- addon/adapters/contributor.js
+++ addon/adapters/contributor.js
@@ -1,4 +1,11 @@
 import OsfAdapter from './osf-adapter';
 
 export default OsfAdapter.extend({
+    buildURL(_, __, snap) {
+        // Modifies URL from /contributors to nodes/<nodeId>/contributors/ to match APIv2 route for creating contributors.
+        var base = this._super(...arguments);
+        var ar = base.split('/');
+        ar.splice(4, 0, 'nodes/' + snap._attributes.nodeId);
+        return ar.join('/');
+    }
 });
