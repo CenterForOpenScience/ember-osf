337545ac2c62aa30838d72a76ff0f37a5363c038
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-16 17:46:04 -0400

    Add comment.

--- addon/serializers/contributor.js
+++ addon/serializers/contributor.js
@@ -4,6 +4,7 @@ export default OsfSerializer.extend({
     serialize: function(snapshot, options) {
         // Restore relationships to serialized data
         var serialized = this._super(snapshot, options);
+        // APIv2 expects contributor information to be nested under relationships.
         serialized.data.relationships = {
             users: {
                 data: {
