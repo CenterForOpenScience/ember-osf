fd49cdfef5bd1c564c68337c08a97ee54b543a99
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-17 15:56:44 -0400

    WIP relationship dirty tracking

--- addon/adapters/node.js
+++ addon/adapters/node.js
@@ -1,13 +1,29 @@
+import Ember from 'ember';
+
 import OsfAdapter from './osf-adapter';
 
 export default OsfAdapter.extend({
-    buildURL(_, __, ___, requestType) {
-        // Embed contributors
-        var base = this._super(...arguments);
-        if (['createRecord', 'updateRecord', 'deleteRecord'].indexOf(requestType) === -1) {
-            return `${base}?embed=contributors`;
+    buildURL(_, __, snapshot, requestType) {
+        if (requestType === 'updateRecord' && !snapshot.record.changedAttributes().length) {
+            var dirtyRelationships = snapshot.record.get('dirtyRelationships');
+            var urls = Object.keys(dirtyRelationships)
+                .filter((rel) => Ember.get(dirtyRelationships, rel))
+                    .map((rel) => {
+			var links = snapshot.record.get(
+			    `links.${Ember.String.underscore(rel)}.links`
+			);
+			return Ember.get(links, 'self.href') || links.related.href;
+		    });
+	    // TODO fixme
+	    return urls[0];
         } else {
-            return base;
+            // Embed contributors
+            var base = this._super(...arguments);
+            if (['createRecord', 'updateRecord', 'deleteRecord'].indexOf(requestType) === -1) {
+                return `${base}?embed=contributors`;
+            } else {
+                return base;
+            }
         }
     }
 });
--- addon/mixins/osf-model.js
+++ addon/mixins/osf-model.js
@@ -7,5 +7,20 @@ import DS from 'ember-data';
 
 export default Ember.Mixin.create({
     links: DS.attr('links'),
-    embeds: DS.attr('embed')
+    embeds: DS.attr('embed'),
+
+    relationshipLinks: DS.attr('links'),
+    dirtyRelationships: {},
+    onLoad: Ember.on('ready', function() {
+	this.eachRelationship((rel) => {
+	    let update = function() {
+		let key = `dirtyRelationships.${rel}`;
+		this.set(key, !Ember.isEmpty(this.get(key)));
+	    }.bind(this);
+
+	    this.get(rel).then(() => {
+		this.addObserver(rel, update);
+	    });
+	});
+    })
 });
