dc4b36da454d022d1c039363a162d793f510aec7
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-17 14:19:32 -0400

    Add action for deleting contributor as well as helper methods to determine if contributor can be deleted.

--- tests/dummy/app/routes/nodes/detail.js
+++ tests/dummy/app/routes/nodes/detail.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
     model(params) {
         return this.store.findRecord('node', params.node_id);
     },
@@ -32,10 +33,65 @@ export default Ember.Route.extend({
                     nodeId: node.id
                 });
                 contributor.save();
+                console.log('Contributor added.');
             } else {
                 console.log('You do not have permissions to add contributors');
             }
+        },
+        deleteContributor(contrib) {
+            var node = this.modelFor(this.routeName);
+            contrib.setProperties({ nodeId: node.id });
+            let user = this.modelFor('application');
+
+            var contribMap = node.get('contributors').content.currentState.reduce(function(newMap, contrib) {
+                newMap[contrib.id] = { permission: contrib._data.permission, bibliographic: contrib._data.bibliographic };
+                return newMap;
+            }, {});
+
+            if (node.get('currentUserPermissions').indexOf('admin') !== -1) {
+                this.attemptContributorRemoval(contrib, contribMap, node);
+            } else {
+                // Non-admins can only attempt to remove themselves as contributors
+                if (contrib.id === user.id) {
+                    this.attemptContributorRemoval(contrib, contribMap, node);
+                } else {
+                    console.log('Non-admins cannot delete other contributors.');
+                }
+            }
+        }
+    },
+    canRemoveContributor(contribRemoving, contribMap) {
+        /** Contributor can only be removed if there is at least one other contributor
+        with admin permissions, and at least one other bibliographic contributor **/
+        var bibliographic = false;
+        var admin = false;
+        for (var contribId in contribMap) {
+            if (contribId === contribRemoving.id) {
+                continue;
+            } else {
+                if (contribMap[contribId].bibliographic) {
+                    bibliographic = true;
+                }
+                if (contribMap[contribId].permission === 'admin') {
+                    admin = true;
+                }
+            }
+        }
+        if (bibliographic && admin) {
+            return true;
+        }
+        return false;
+    },
+
+    attemptContributorRemoval(contrib, contribMap, node) {
+        if (this.canRemoveContributor(contrib, contribMap)) {
+            node.get('contributors').removeObject(contrib);
+            contrib.deleteRecord();
+            node.save();
+            contrib.save();
+            console.log('Contributor removed.');
+        } else {
+            console.log('Cannot remove contributor');
         }
     }
-
 });
