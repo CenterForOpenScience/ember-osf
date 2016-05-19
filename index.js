07155345557ad70ba3452da34beba31d45ceda92
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-19 16:34:46 -0400

    Add render urls to config.

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ module.exports = {
             ENV.OSF.url = 'http://localhost:5000/';
             ENV.OSF.apiUrl = 'http://localhost:8000';
             ENV.OSF.authUrl = 'http://localhost:8080/oauth2/profile';
+            ENV.OSF.renderUrl = 'http://localhost:7778/render';
 
             ENV.OSF.accessToken = SETTINGS.PERSONAL_ACCESS_TOKEN;
             ENV.OSF.isLocal = true;
@@ -36,22 +37,26 @@ module.exports = {
             ENV.OSF.url = 'https://staging.osf.io/';
             ENV.OSF.apiUrl = 'https://staging-api.osf.io';
             ENV.OSF.authUrl = 'https://staging-accounts.osf.io/oauth2/authorize';
+            ENV.OSF.renderUrl = 'http://staging-mfr.osf.io/render';
         }
         if (BACKEND === 'stage2') {
             ENV.OSF.url = 'https://staging2.osf.io/';
             ENV.OSF.apiUrl = 'https://staging2-api.osf.io';
             ENV.OSF.authUrl = 'https://staging2-accounts.osf.io/oauth2/authorize';
+            ENV.OSF.renderUrl = 'http://staging2-mfr.osf.io/render';
         }
         if (BACKEND === 'test') {
             ENV.OSF.url = 'https://test.osf.io/';
             ENV.OSF.apiUrl = 'https://test-api.osf.io';
             ENV.OSF.authUrl = 'https://test-accounts.osf.io/oauth2/authorize';
+            ENV.OSF.renderUrl = 'http://test-mfr.osf.io/render';
         }
         if (BACKEND === 'prod') {
             console.log(`WARNING: you\'ve specified production as a backend. Please do not use production for testing or development purposes`);
             ENV.OSF.url = 'https://osf.io/';
             ENV.OSF.apiUrl = 'https://api.osf.io';
             ENV.OSF.authUrl = 'https://accounts.osf.io/oauth2/authorize';
+            ENV.OSF.renderUrl = 'http://mfr.osf.io/render';
         }
         ENV['ember-simple-auth'] = {
             authorizer: 'authorizer:osf-token'
