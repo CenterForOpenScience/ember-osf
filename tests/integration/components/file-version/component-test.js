7a9e50d912eb9588f45b38fe4bd555c429a3d15f
Saman Ehsan <se8ea@virginia.edu>
2016-05-19 14:51:44 -0400

    Fix failing test

--- tests/integration/components/file-version/component-test.js
+++ tests/integration/components/file-version/component-test.js
@@ -8,17 +8,9 @@ moduleForComponent('file-version', 'Integration | Component | file version', {
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('version', {'id': '1', 'size': 125, 'contentType': 'text'});
+  this.render(hbs`{{file-version model=version}}`);
 
-  this.render(hbs`{{file-version}}`);
+  assert.equal(this.$().text().trim(), 'ID: 1\n  Size: 125\n  Content type: text');
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#file-version}}
-      template block text
-    {{/file-version}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
