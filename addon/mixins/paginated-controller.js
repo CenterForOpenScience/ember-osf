ef83f83d98e0b1901fd9d35c6dd331b03a6c08a7
Andy Boughton <abought@gmail.com>
2016-05-19 13:26:06 -0400

    Simple increment/decrement controls
    [ci skip]

--- addon/mixins/paginated-controller.js
+++ addon/mixins/paginated-controller.js
@@ -3,15 +3,26 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
     queryParams: ['page', 'page_size'],
     // TODO: Confirm that setting to null causes server to use its default values
-    page: null,
+    page: 1,
     page_size: null,
-    
+
+    // Placeholders, may use pagination metadata to track this instead
+    firstPage: null,
+    lastPage: null,
+
     actions: {
-        prev() {
-            console.log('previous');
+        previous() {
+            // TODO: Add bounds checking somewhere
+            this.decrementProperty('page', 1);
+            console.log('decrement to previous');
         },
         next() {
-            console.log('next');
+            this.incrementProperty('page', 1);
+            console.log('increment to next');
+        },
+        goToPage(pageNumber) {
+            this.set('page', pageNumber);
+            console.log('Went to page: ', this.get('page'));
         }
     }
 });
--- tests/dummy/app/controllers/users/list.js
+++ tests/dummy/app/controllers/users/list.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-import PaginatedControllerMixin from  'ember-osf/mixins/paginated-route';
+import PaginatedControllerMixin from  'ember-osf/mixins/paginated-controller';
 
 export default Ember.Controller.extend(PaginatedControllerMixin, {
 });
