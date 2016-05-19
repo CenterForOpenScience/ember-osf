c7d4513bb6d528a292b0bce99b97438ffceeb4a6
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-19 10:42:09 -0400

    JS styling fixes

--- tests/dummy/app/controllers/collections/index.js
+++ tests/dummy/app/controllers/collections/index.js
@@ -1,12 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  actions: {
-    createCollection: function(title) {
-      var collection = this.store.createRecord('collection',{
-        title: title,
-      });
-      collection.save();
+    actions: {
+        createCollection: function(title) {
+            var collection = this.store.createRecord('collection', {
+                title: title
+            });
+            collection.save();
+        }
     }
-  }
 });
--- tests/dummy/app/routes/collections.js
+++ tests/dummy/app/routes/collections.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin,{
-  store: Ember.inject.service(),
-  session: Ember.inject.service(),
-  model() {
-    return this.get('store').findAll('collection');
-  }
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+    store: Ember.inject.service(),
+    session: Ember.inject.service(),
+    model() {
+        return this.get('store').findAll('collection');
+    }
 });
