5fec33458c65b63016e462bbbcfa678b228e9d4a
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-19 16:36:14 -0400

    Add file-renderer component.

---
+++ tests/integration/components/file-renderer/component-test.js
@@ -0,0 +1,24 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('file-renderer', 'Integration | Component | file renderer', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{file-renderer}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#file-renderer}}
+      template block text
+    {{/file-renderer}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
