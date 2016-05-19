e4881f77be9fefb1bddb8d63602c73dbd7324551
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-19 10:22:10 -0400

    Clean up merge for collections work
    h/t @gembarrett

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -34,6 +34,11 @@ Router.map(function() {
         path: '/file/:file_id'
     });
     this.route('profile');
+    this.route('collections', function() {
+        this.route('detail', {
+            path: '/:collection_id'
+        });
+    });
 });
 
 export default Router;
