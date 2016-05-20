0e0e418c4dbe33b10aee8a7c11740a9f04216676
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-20 14:28:23 -0400

    Fix bad indents

--- addon/adapters/contributor.js
+++ addon/adapters/contributor.js
@@ -2,7 +2,7 @@ import OsfAdapter from './osf-adapter';
 
 export default OsfAdapter.extend({
     buildURL(_, __, snap) {
-	// TOOD: delete me when https://github.com/CenterForOpenScience/ember-osf/pull/34 is merged
+        // TOOD: delete me when https://github.com/CenterForOpenScience/ember-osf/pull/34 is merged
 
         // Modifies URL from /contributors to nodes/<nodeId>/contributors/ to match APIv2 route for creating contributors.
         var base = this._super(...arguments);
