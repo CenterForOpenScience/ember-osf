aa0f42b7baddf391a9c9f03ee9b9159b15e2086e
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-20 09:51:48 -0400

    model -> version; slight tweak to file-version semantics

--- tests/integration/components/file-version/component-test.js
+++ tests/integration/components/file-version/component-test.js
@@ -9,7 +9,7 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
   this.set('version', {'id': '1', 'size': 125, 'contentType': 'text'});
-  this.render(hbs`{{file-version model=version}}`);
+  this.render(hbs`{{file-version version=version}}`);
 
   assert.equal(this.$().text().trim(), 'ID: 1\n  Size: 125\n  Content type: text');
 
