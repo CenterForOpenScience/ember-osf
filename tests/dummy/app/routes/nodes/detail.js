ff62b831b7666278b3ea661e681098c7bdeed0ae
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-23 12:41:11 -0400

    Add actions overwritten by merge.

--- tests/dummy/app/routes/nodes/detail.js
+++ tests/dummy/app/routes/nodes/detail.js
@@ -21,14 +21,76 @@ export default Ember.Route.extend({
             // console.log('Will edit title from', this.modelFor(this.routeName).get('title'), ' to ', value);
             var node = this.modelFor(this.routeName);
             if (node.get('currentUserPermissions').indexOf('write') !== -1) {
-                console.log(title, category, description, this.routeName);
                 if (title) {node.set('title', title);}
                 if (category) {node.set('category', category);}
                 if (description) {node.set('description', description);}
-                if (isPublic) {node.set('public', isPublic);}
+                if (isPublic !== null) {node.set('public', isPublic);}
                 node.save();
             } else {
                 console.log('You do not have permissions to edit this node');
+            }
+        },
+        addContributor(contribId, permission, bibliographic) {
+            var node = this.modelFor(this.routeName);
+            if (contribId) {
+                if (node.get('currentUserPermissions').indexOf('admin') !== -1) {
+                    var contributor = this.store.createRecord('contributor', {
+                        id: contribId,
+                        permission: permission,
+                        bibliographic: bibliographic,
+                        nodeId: node.id
+                    });
+                    contributor.save();
+                    console.log('Contributor added.');
+                } else {
+                    console.log('You do not have permissions to add contributors');
+                }
+            } else {
+                console.log('User ID must be specified.');
+            }
+        },
+        updateContributors(editedPermissions, editedBibliographic) {
+            var node = this.modelFor(this.routeName);
+            var contribMap = this.generateContributorMap(node.get('contributors'));
+            let user = this.modelFor('application');
+
+            for (var contrib in editedPermissions) {
+                contribMap[contrib].permission = editedPermissions[contrib];
+            }
+
+            for (var c in editedBibliographic) {
+                contribMap[c].bibliographic = editedBibliographic[c];
+            }
+
+            if (node.get('currentUserPermissions').indexOf('admin') !== -1) {
+                this.attemptContributorsUpdate(contribMap, node, editedPermissions, editedBibliographic);
+            } else {
+                // Non-admins can only attempt to remove themselves as contributors
+                if (contrib.id === user.id) {
+                    this.attemptContributorsUpdate(contribMap, node, editedPermissions,
+                      editedBibliographic);
+                } else {
+                    console.log('Non-admins cannot update other contributors.');
+                }
+            }
+
+        },
+        deleteContributor(contrib) {
+            var node = this.modelFor(this.routeName);
+            contrib.setProperties({ nodeId: node.id });
+            let user = this.modelFor('application');
+
+            var contribMap = this.generateContributorMap(node.get('contributors'));
+
+            if (node.get('currentUserPermissions').indexOf('admin') !== -1) {
+                this.attemptContributorRemoval(contrib, contribMap);
+            } else {
+                // Non-admins can only attempt to remove themselves as contributors
+                if (contrib.id === user.id) {
+                    this.attemptContributorRemoval(contrib, contribMap);
+                } else {
+                    console.log('Non-admins cannot delete other contributors.');
+                }
             }
         },
         addChildren(title, description, category) {
@@ -60,6 +122,36 @@ export default Ember.Route.extend({
             }
         }
 
+    },
+    generateContributorMap(contributors) {
+        // Maps all node contributors to format {contribID: {permission: "read|write|admin", bibliographic: "true|false"}}
+        var contribMap = contributors.content.currentState.reduce(function(newMap, contrib) {
+            newMap[contrib.id] = { permission: contrib._data.permission, bibliographic: contrib._data.bibliographic };
+            return newMap;
+        }, {});
+        return contribMap;
+    },
+    canModifyContributor(contribRemoving, contribMap) {
+        /** Checks to see if contributor(s) can be updated/removed. Contributor can only be updated/removed
+        if there is at least one other contributor with admin permissions, and at least one other bibliographic contributor **/
+        var bibliographic = false;
+        var admin = false;
+        for (var contribId in contribMap) {
+            if (contribRemoving && contribId === contribRemoving.id) {
+                continue;
+            } else {
+                if (contribMap[contribId].bibliographic) {
+                    bibliographic = true;
+                }
+                if (contribMap[contribId].permission === 'admin') {
+                    admin = true;
+                }
+            }
+        }
+        if (bibliographic && admin) {
+            return true;
+        }
+        return false;
     },
 
     attemptContributorsUpdate(contribMap, node, editedPermissions, editedBibliographic) {
