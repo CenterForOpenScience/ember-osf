f32b4037182481fd1669a5a01644816c6d50b53b
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-24 10:00:42 -0400

    Add additional query parameters to url sent to MFR.

--- addon/helpers/build-mfr-url.js
+++ addon/helpers/build-mfr-url.js
@@ -4,7 +4,7 @@ import config from 'ember-get-config';
 export function buildMfrUrl(params/*, hash*/) {
     var base = config.OSF.renderUrl;
     var download = params[0];
-    var renderUrl = base + '?url=' + encodeURIComponent(download);
+    var renderUrl = base + '?url=' + encodeURIComponent(download + '?direct&mode=render&initialWidth=766&childId=mfrIframe');
     return renderUrl;
 }
 
