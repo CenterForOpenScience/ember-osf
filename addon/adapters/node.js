6afedab88cacf308cd0d9dbeed8245484c017231
Henrique Moco <hmoco94@vt.edu>
2016-05-24 14:41:35 -0400

    remove children model, adapter and serializer

--- addon/adapters/node.js
+++ addon/adapters/node.js
@@ -1,7 +1,7 @@
 import OsfAdapter from './osf-adapter';
 
 export default OsfAdapter.extend({
-    buildURL(_, __, snapshot, requestType) {
+    buildURL(_, __, ___, requestType) {
         // Embed contributors
         var base = this._super(...arguments);
         if (['createRecord', 'updateRecord', 'deleteRecord'].indexOf(requestType) === -1) {
--- addon/serializers/children.js
+++
@@ -1,8 +0,0 @@
-import OsfSerializer from './osf-serializer';
-
-export default OsfSerializer.extend({
-    payloadKeyFromModelName: function() {
-        // Overrides type 'children' with type 'nodes'. APIv2 expecting type 'nodes'.
-        return 'nodes';
-    }
-});
