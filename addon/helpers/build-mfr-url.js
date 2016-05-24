ab34a1707f8c75c2add64994296f5f2b9aa4a491
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-24 09:18:44 -0400

    Encode download url before passing to MFR.

--- addon/helpers/build-mfr-url.js
+++ addon/helpers/build-mfr-url.js
@@ -4,9 +4,8 @@ import config from 'ember-get-config';
 export function buildMfrUrl(params/*, hash*/) {
     var base = config.OSF.renderUrl;
     var download = params[0];
-    var renderUrl = base + '?url=' + download;
-    // return renderUrl;
-    return 'http://localhost:7778/render?url=http://localhost:5000/zps63/?action=download%26direct%26mode=render&initialWidth=766&childId=mfrIframe';
+    var renderUrl = base + '?url=' + encodeURIComponent(download);
+    return renderUrl;
 }
 
 export default Ember.Helper.helper(buildMfrUrl);
