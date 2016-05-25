3326db119829147b3d77ae59b7ccf4a5b19a6b57
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-25 12:04:12 -0400

    Add tests for the file-renderer component.

--- tests/integration/components/file-renderer/component-test.js
+++ tests/integration/components/file-renderer/component-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import config from 'ember-get-config';
 
 moduleForComponent('file-renderer', 'Integration | Component | file renderer', {
   integration: true
@@ -20,4 +21,31 @@ test('it renders', function(assert) {
   `);
 
   assert.equal(this.$().text().trim(), '');
+});
+
+test('file rendering defaults', function(assert) {
+
+  this.render(hbs`
+    {{#file-renderer}}
+    {{/file-renderer}}
+  `);
+
+  assert.equal(this.$('iframe').attr('height'), '100%');
+  assert.equal(this.$('iframe').attr('width'), '100%');
+  assert.equal(this.$('iframe').attr('src'), config.OSF.renderUrl + "?url=" + encodeURIComponent(null + '?direct&mode=render&initialWidth=766'));
+});
+
+test('specify file rendering parameters', function(assert) {
+  this.set('download', 'http://cos.io/');
+  this.set('height', '500');
+  this.set('width', '500');
+
+  this.render(hbs`
+    {{#file-renderer download=download height=height width=width}}
+    {{/file-renderer}}
+  `);
+
+  assert.equal(this.$('iframe').attr('height'), '500');
+  assert.equal(this.$('iframe').attr('width'), '500');
+  assert.equal(this.$('iframe').attr('src'), config.OSF.renderUrl + "?url=" + encodeURIComponent('http://cos.io/' + '?direct&mode=render&initialWidth=766'));
 });
