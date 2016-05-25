76c840c13513cf67e88a81b6a4ed9d9a8c592d45
Abram Booth <abram@cos.io>
2016-05-25 13:20:41 -0400

    Prereg page to demonstrate file-chooser
    Doesn't do anything, it's just a demo of using file-chooser as a block
    component.
    
    [skip ci]

--- tests/dummy/app/routes/prereg.js
+++ tests/dummy/app/routes/prereg.js
@@ -1,6 +1,22 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-// TODO: really simple use case for file-chooser
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+    fileManager: Ember.inject.service(),
+    actions: {
+        preregister(name, files) {
+            let store = this.get('store');
 
+            let fileNames = files.mapBy('name').join(', ');
+
+            // TODO: This page is just a file-chooser demo at this point
+            alert(`Files: ${fileNames}`);
+
+            // store.createRecord('node', { title: name, ... })
+            
+            // for (let file of files) {
+            //     this.get('fileManager').uploadFile(...)
+            // }
+        }
+    }
 });
