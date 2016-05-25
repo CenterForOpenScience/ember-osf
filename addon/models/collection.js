d0df489d8350c8dd3c976eca889d5550cd588e99
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-25 12:35:09 -0400

    Add bookmarks attribute.

--- addon/models/collection.js
+++ addon/models/collection.js
@@ -6,6 +6,7 @@ export default DS.Model.extend(OsfModel, {
     title: DS.attr('string'),
     dateCreated: DS.attr('date'),
     dateModified: DS.attr('date'),
+    bookmarks: DS.attr('boolean'),
 
     // nodeLinks: DS.hasMany('node-pointers'),
     // linkedNodes: DS.hasMany('node-pointers')
