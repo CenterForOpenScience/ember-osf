b927dd3f0daa818110aa60e6568e420d3d824554
Andy Boughton <abought@gmail.com>
2016-05-25 16:55:03 -0400

    Connect locales and sample translation of signup page
    [#EOSF-25]
    [ci skip]

--- app/locales/en-us/translations.js
+++ app/locales/en-us/translations.js
@@ -1,11 +1,28 @@
 export default {
-  // "some.translation.key": "Text for some.translation.key",
-  //
-  // "a": {
-  //   "nested": {
-  //     "key": "Text for a.nested.key"
-  //   }
-  // },
-  //
-  // "key.with.interpolation": "Text with {{anInterpolation}}"
+    // "some.translation.key": "Text for some.translation.key",
+    //
+    // "a": {
+    //   "nested": {
+    //     "key": "Text for a.nested.key"
+    //   }
+    // },
+    //
+    // "key.with.interpolation": "Text with {{anInterpolation}}"
+    login: {
+        formTitle: 'Login',
+        labelRememberMe: 'Remember me',
+        labelEmail: 'Email',
+        labelPassword: 'Password',
+        buttonSignIn: 'Sign in'
+    },
+    signup: {
+        formTitle: 'Create a free account',
+        labelFullName: 'Full name',
+        labelEmail: 'Email',
+        labelConfirmEmail: 'Confirm Email',
+        placeholderConfirmEmail: 'Re-enter email',
+        labelPassword: 'Password',
+        textTosNotice: 'By clicking "Create account", you agree to our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/TERMS_OF_USE.md">Terms</a> and that you have read our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md">Privacy Policy</a>, including our information on <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md#f-cookies">Cookie Use</a>.',
+        buttonCreateAccount: 'Create account'
+    }
 };
