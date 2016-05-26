03c785f2bcb93656ba594b1de7ea5828a07b5c40
Andy Boughton <abought@gmail.com>
2016-05-26 19:13:23 -0400

    Allow addon and app translations to coexist
    Adds new instance initializer for addons, as well as proof of concept locale file for app
    
    [ci skip]

---
+++ addon/locales/en-us/translations.js
@@ -0,0 +1,30 @@
+export default {
+    // "some.translation.key": "Text for some.translation.key",
+    //
+    // "a": {
+    //   "nested": {
+    //     "key": "Text for a.nested.key"
+    //   }
+    // },
+    //
+    // "key.with.interpolation": "Text with {{anInterpolation}}"
+    eosf: {
+        loginForm: {
+            formTitle: 'Login',
+            labelInputEmail: 'Email',
+            labelInputPassword: 'Password',
+            labelInputRemember: 'Remember me',
+            buttonSubmit: 'Sign in'
+        },
+        signup: {
+            formTitle: 'Create a free account',
+            labelInputName: 'Full name',
+            labelInputEmail: 'Email',
+            labelConfirmEmail: 'Confirm Email',
+            placeholderConfirmEmail: 'Re-enter email',
+            labelInputPassword: 'Password',
+            textTosNotice: 'By clicking "Create account", you agree to our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/TERMS_OF_USE.md">Terms</a> and that you have read our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md">Privacy Policy</a>, including our information on <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md#f-cookies">Cookie Use</a>.',
+            buttonSubmit: 'Create account'
+        }
+    }
+};
---
+++ app/instance-initializers/ember-osf.js
@@ -0,0 +1,1 @@
+export { default, initialize } from 'ember-osf/instance-initializers/ember-osf';
