ae416a7f256c1544755099b41eafb2b6a64510e8
Andy Boughton <abought@gmail.com>
2016-05-26 10:01:42 -0400

    Namespace translations for addon under `eosf`
    Next: test avoidance of conflicts with a project

--- app/locales/en-us/translations.js
+++ app/locales/en-us/translations.js
@@ -8,21 +8,23 @@ export default {
     // },
     //
     // "key.with.interpolation": "Text with {{anInterpolation}}"
-    login: {
-        formTitle: 'Login',
-        labelRememberMe: 'Remember me',
-        labelEmail: 'Email',
-        labelPassword: 'Password',
-        buttonSignIn: 'Sign in'
-    },
-    signup: {
-        formTitle: 'Create a free account',
-        labelFullName: 'Full name',
-        labelEmail: 'Email',
-        labelConfirmEmail: 'Confirm Email',
-        placeholderConfirmEmail: 'Re-enter email',
-        labelPassword: 'Password',
-        textTosNotice: 'By clicking "Create account", you agree to our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/TERMS_OF_USE.md">Terms</a> and that you have read our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md">Privacy Policy</a>, including our information on <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md#f-cookies">Cookie Use</a>.',
-        buttonCreateAccount: 'Create account'
+    eosf: {
+        login: {
+            formTitle: 'Login',
+            labelRememberMe: 'Remember me',
+            labelEmail: 'Email',
+            labelPassword: 'Password',
+            buttonSignIn: 'Sign in'
+        },
+        signup: {
+            formTitle: 'Create a free account',
+            labelFullName: 'Full name',
+            labelEmail: 'Email',
+            labelConfirmEmail: 'Confirm Email',
+            placeholderConfirmEmail: 'Re-enter email',
+            labelPassword: 'Password',
+            textTosNotice: 'By clicking "Create account", you agree to our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/TERMS_OF_USE.md">Terms</a> and that you have read our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md">Privacy Policy</a>, including our information on <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md#f-cookies">Cookie Use</a>.',
+            buttonCreateAccount: 'Create account'
+        }
     }
 };
