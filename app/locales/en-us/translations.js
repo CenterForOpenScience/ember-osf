8b22885573e9223ad0de2174121a4428ab8683ce
Andy Boughton <abought@gmail.com>
2016-05-26 18:54:32 -0400

    Impose naming conventions and uniqueness on element ids
    Goal: translation strings should be named abstractly after their role in the document, rather than based on the words you're replacing in english.
    
    Element IDs should be unique even when two of a component are used on the same page
    
    [ci skip]

--- app/locales/en-us/translations.js
+++ app/locales/en-us/translations.js
@@ -9,22 +9,22 @@ export default {
     //
     // "key.with.interpolation": "Text with {{anInterpolation}}"
     eosf: {
-        login: {
+        loginForm: {
             formTitle: 'Login',
-            labelRememberMe: 'Remember me',
-            labelEmail: 'Email',
-            labelPassword: 'Password',
-            buttonSignIn: 'Sign in'
+            labelInputEmail: 'Email',
+            labelInputPassword: 'Password',
+            labelInputRemember: 'Remember me',
+            buttonSubmit: 'Sign in'
         },
         signup: {
             formTitle: 'Create a free account',
-            labelFullName: 'Full name',
-            labelEmail: 'Email',
+            labelInputName: 'Full name',
+            labelInputEmail: 'Email',
             labelConfirmEmail: 'Confirm Email',
             placeholderConfirmEmail: 'Re-enter email',
-            labelPassword: 'Password',
+            labelInputPassword: 'Password',
             textTosNotice: 'By clicking "Create account", you agree to our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/TERMS_OF_USE.md">Terms</a> and that you have read our <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md">Privacy Policy</a>, including our information on <a href="https://github.com/CenterForOpenScience/centerforopenscience.org/blob/master/PRIVACY_POLICY.md#f-cookies">Cookie Use</a>.',
-            buttonCreateAccount: 'Create account'
+            buttonSubmit: 'Create account'
         }
     }
 };
