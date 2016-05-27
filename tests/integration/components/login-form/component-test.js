83aae1f38cb20cb1e32f903f5655db90d3d0e7e8
Andy Boughton <abought@gmail.com>
2016-05-27 14:00:05 -0400

    Demonstrate a shameful hack that fixes test with element ids.
    Also some formatting changes, but more needed for check-style
    
    [ci skip]

--- tests/integration/components/login-form/component-test.js
+++ tests/integration/components/login-form/component-test.js
@@ -2,19 +2,20 @@ import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('login-form', 'Integration | Component | login form', {
-  integration: true
+    integration: true,
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{login-form}}`);
+    this.render(hbs`{{login-form}}`);
 
-  assert.equal(this.$('label[for=inputEmail3]').text(), 'Email');
-  assert.ok(this.$('input[id=inputEmail3]'));
-  assert.equal(this.$('label[for=inputPassword3]').text(), 'Password');
-  assert.ok(this.$('input[id=inputPassword3]'));
-  assert.ok(this.$('label:contains("Remember me)'));
+    // "This" is not the component instance itself, but rather an object with a different GUID
+    // We can hack it but not proudly
+    let someID = this.$().children().attr('id');
 
+    // Test that the expected fields are present (accounting for unique IDs)
+    assert.ok(this.$(`input[id=${someID}-login-input-email]`).length);
+    assert.ok(this.$(`input[id=${someID}-login-input-password]`).length);
 });
