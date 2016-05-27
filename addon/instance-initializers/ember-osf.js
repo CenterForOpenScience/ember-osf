a2b9c65f6dbe178b1cb9aa25a815c24c835dd0c6
Andy Boughton <abought@gmail.com>
2016-05-27 14:50:18 -0400

    Appease linters.
    [ci skip]

--- addon/instance-initializers/ember-osf.js
+++ addon/instance-initializers/ember-osf.js
@@ -2,9 +2,9 @@
 Automatically expose translations for addon in a way that can be merged in with app
     https://github.com/jamesarosen/ember-i18n/issues/255
  */
-import enUS from "ember-osf/locales/en-us/translations";
+import enUS from 'ember-osf/locales/en-us/translations';
 
-export function initialize({container}) {
+export function initialize({ container }) {
     const i18n = container.lookup('service:i18n');
     i18n.addTranslations('en-US', enUS);
 }
