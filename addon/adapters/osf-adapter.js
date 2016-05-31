6c5618b9ec132c2c870adca63354783811b5200e
Sam Chrisinger <s.chrisinger@gmail.com>
2016-05-31 11:02:09 -0400

    Revert "Add extra check for promises.length"
    This reverts commit bf6f82f79cf9873ff1eca043b1f832cf7fba6d97.

--- addon/adapters/osf-adapter.js
+++ addon/adapters/osf-adapter.js
@@ -81,7 +81,7 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
             });
         }
         if (Object.keys(snapshot.record.changedAttributes()).length) {
-            if (promises && promises.length) {
+            if (promises) {
                 return this._super(...arguments).then(response => Ember.RSVP.allSettled(promises).then(() => response));
             }
             return this._super(...arguments);
