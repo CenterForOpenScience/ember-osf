4b9cea79f95d8f240f2f22411cbb58056dc116f3
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-31 12:02:17 -0400

    For buildUrl, add different handling when deleting record.

--- addon/adapters/osf-adapter.js
+++ addon/adapters/osf-adapter.js
@@ -11,10 +11,13 @@ export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
     authorizer: 'authorizer:osf-token',
     host: config.OSF.apiUrl,
     namespace: config.OSF.apiNamespace,
-    buildURL() {
+    buildURL(modelName, id, snapshot, requestType) {
         // Fix issue where CORS request failed on 301s: Ember does not seem to append trailing
         // slash to URLs for single documents, but DRF redirects to force a trailing slash
         var url = this._super(...arguments);
+        if (requestType === 'deleteRecord') {
+            url = snapshot.record.get('links.self');
+        }
         if (url.lastIndexOf('/') !== url.length - 1) {
             url += '/';
         }
