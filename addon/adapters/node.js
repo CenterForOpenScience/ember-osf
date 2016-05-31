0adcfcb0525e6b2040ced8dba6dd7eb313988c03
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-05-31 11:58:35 -0400

    Embed node links in node detail request.

--- addon/adapters/node.js
+++ addon/adapters/node.js
@@ -2,10 +2,10 @@ import OsfAdapter from './osf-adapter';
 
 export default OsfAdapter.extend({
     buildURL(_, __, ___, requestType) {
-        // Embed contributors
+        // Embed contributors and node_links
         var base = this._super(...arguments);
         if (['createRecord', 'updateRecord', 'deleteRecord'].indexOf(requestType) === -1) {
-            return `${base}?embed=contributors`;
+            return `${base}?embed=contributors&embed=node_links`;
         } else {
             return base;
         }
