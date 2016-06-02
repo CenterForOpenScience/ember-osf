e1ae5cac9880d334ab02ed95425980cc8d1ceb19
Andy Boughton <abought@gmail.com>
2016-06-02 14:03:10 -0400

    Merge different login components; layout fixes
    Components shouldn't specify position-width in the outer grid
    
    [#EOSF-37]
    [ci skip]

--- addon/components/login-form/component.js
+++ addon/components/login-form/component.js
@@ -2,5 +2,7 @@ import Ember from 'ember';
 import layout from './template';
 
 export default Ember.Component.extend({
-    layout
+    layout,
+    // Where to send a POST request for login
+    submitUrl: 'http://localhost:8080/login?service=http://localhost:4200/&amp;auto=true'  // TODO: Don't hard-code
 });
