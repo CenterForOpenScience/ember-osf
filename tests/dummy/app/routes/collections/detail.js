31a3892d83f0a13a163f3013def01ca63823c38b
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-03 10:14:23 -0400

    Add action for adding collection.

--- tests/dummy/app/routes/collections/detail.js
+++ tests/dummy/app/routes/collections/detail.js
@@ -4,5 +4,14 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     model(params) {
         return this.store.findRecord('collection', params.collection_id);
+    },
+    actions: {
+        addNodeToCollection(projectId) {
+            this.store.findRecord('node', projectId).then(node => {
+                var collection = this.modelFor(this.routeName);
+                collection.get('linkedNodes').pushObject(node);
+                collection.save();
+            });
+        },
     }
 });
