4570ca04059c31d6150f315b1384979cbde15934
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-06 11:14:20 -0400

    Add new unregistered contributor field to contributor model.

--- addon/models/contributor.js
+++ addon/models/contributor.js
@@ -6,5 +6,6 @@ export default OsfModel.extend({
     bibliographic: DS.attr('boolean'),
     permission: DS.attr('string'),
     users: DS.belongsTo('user'),
-    nodeId: DS.attr('string')
+    nodeId: DS.attr('string'),
+    unregisteredContributor: DS.attr('string')
 });
