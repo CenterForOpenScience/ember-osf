d60ed6861382f47527ebfa837a3814e57de5bc2e
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-06 10:54:13 -0400

    Remove unnecessary embedding of contributors and node links for node.

--- addon/adapters/node.js
+++ addon/adapters/node.js
@@ -1,13 +1,3 @@
 import OsfAdapter from './osf-adapter';
 
-export default OsfAdapter.extend({
-    buildURL(_, __, ___, requestType) {
-        // Embed contributors and node_links
-        var base = this._super(...arguments);
-        if (['createRecord', 'updateRecord', 'deleteRecord'].indexOf(requestType) === -1) {
-            return `${base}?embed=contributors&embed=node_links`;
-        } else {
-            return base;
-        }
-    }
-});
+export default OsfAdapter.extend({});
