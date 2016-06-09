70abc3debebff18e11561c6cb2ca359d25d83932
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-09 16:45:06 -0400

    Put addATag and removeATag actions on file controller.

--- tests/dummy/app/controllers/nodes/detail/files/provider/file.js
+++ tests/dummy/app/controllers/nodes/detail/files/provider/file.js
@@ -5,6 +5,21 @@ export default Ember.Controller.extend({
         reloadFiles() {
             this.transitionToRoute('nodes.detail.files.provider',
                 this.get('node'), this.model.get('provider'));
+        },
+        addATag(tag) {
+            var file = this.get('model');
+            var currentTags = file.get('tags').slice(0);
+            Ember.A(currentTags);
+            currentTags.pushObject(tag);
+            file.set('tags', currentTags);
+            file.save();
+        },
+        removeATag(tag) {
+            var node = this.get('model');
+            var currentTags = node.get('tags').slice(0);
+            currentTags.splice(currentTags.indexOf(tag), 1);
+            node.set('tags', currentTags);
+            node.save();
         }
     }
 });
