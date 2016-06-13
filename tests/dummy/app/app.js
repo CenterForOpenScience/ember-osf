d485d2fe6b37a0117afd9110229be8ef68edd9fd
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-13 19:59:31 -0400

    Possibly improve runloop debuggability?

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -6,6 +6,7 @@ import config from './config/environment';
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
+Ember.run.backburner.DEBUG = true;
 
 App = Ember.Application.extend({
     modulePrefix: config.modulePrefix,
