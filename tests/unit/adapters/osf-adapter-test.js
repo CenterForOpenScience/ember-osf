8c097beee1605d17cafb9dec93c6ade628270080
Henrique Moco <hmoco94@vt.edu>
2016-06-13 09:57:55 -0400

    start writing tests

--- tests/unit/adapters/osf-adapter-test.js
+++ tests/unit/adapters/osf-adapter-test.js
@@ -1,12 +1,36 @@
 import { moduleFor, test } from 'ember-qunit';
+import FactoryGuy, {manualSetup, make, mockSetup }  from 'ember-data-factory-guy';
 
 moduleFor('adapter:osf-adapter', 'Unit | Adapter | osf adapter', {
   // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
+  needs: ['model:node', 'model:contributor', 'model:user', 'transform:links', 'transform:embed', 'model:institution',
+  'model:comment', 'model:file-version', 'model:file-provider', 'model:file', 'model:collection', 'model:log', 'model:node-link',
+    'model:registration', 'model:comment-report'],
+
+  beforeEach(){
+      manualSetup(this.container);
+      mockSetup();
+  }
 });
+
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
   let adapter = this.subject();
   assert.ok(adapter);
 });
+
+test('contributor addition payload is correctly formatted', function(assert){
+    let node = make('node');
+    node.save()
+    let contributor = make('contributor');
+    let adapter = this.subject();
+    adapter.ajax = function(){
+        console.log('poo!!!!')
+    };
+    node.get('contributors').pushObject(contributor);
+    //node._internalModel.unloadRecord(); //<- flushes all changes, and makes .save() do nothing
+    node.save();
+    assert.ok(true);
+})
