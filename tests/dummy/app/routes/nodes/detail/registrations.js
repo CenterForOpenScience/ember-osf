c5e7d4d6a9b05782aafefd346b6921f54d88dde9
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-14 11:47:33 -0400

    Add routes for node registrations and node draft registrations.

---
+++ tests/dummy/app/routes/nodes/detail/registrations.js
@@ -0,0 +1,8 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model() {
+        let node = this.modelFor('nodes.detail');
+        return node.get('registrations');
+    },
+});
