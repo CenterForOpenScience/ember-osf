8a53f350fed72c099f9da0716dd54ed9213c60fe
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-14 11:15:06 -0400

    Add draft-registration model.

---
+++ addon/models/draft-registration.js
@@ -0,0 +1,18 @@
+import DS from 'ember-data';
+import OsfModel from './osf-model';
+
+export default OsfModel.extend({
+    registrationSupplement: DS.attr('string'),
+    registrationMetadata: DS.attr(),
+    datetimeInitiated: DS.attr('date'),
+    datetimeUpdated: DS.attr('date'),
+    branchedFrom: DS.belongsTo('node', {
+        inverse: null
+    }),
+    initiator: DS.belongsTo('user', {
+        inverse: null
+    }),
+    registrationSchema: DS.belongsTo('metaschema', {
+        inverse: null
+    })
+});
