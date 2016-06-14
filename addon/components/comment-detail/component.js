ffb45125c4f1caf34a779a131fe1e6845018df45
Andy Boughton <abought@gmail.com>
2016-06-13 22:05:20 -0400

    Less boilerplate with closure actions
    [ci skip]
    [#EOSF-20]

--- addon/components/comment-detail/component.js
+++ addon/components/comment-detail/component.js
@@ -3,20 +3,5 @@ import layout from './template';
 
 export default Ember.Component.extend({
     layout,
-    comment: null,
-
-    actions: {
-        editComment(comment) {
-            this.sendAction('editComment', comment);
-        },
-        deleteComment(comment) {
-            this.sendAction('deleteComment', comment);
-        },
-        restoreComment(comment) {
-            this.sendAction('restoreComment', comment);
-        },
-        reportComment(comment) {
-            this.sendAction('reportComment', comment);
-        }
-    }
+    comment: null
 });
