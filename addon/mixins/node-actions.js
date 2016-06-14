ac597ea0a23ddd7f8407df93008462338f111e3c
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-14 14:26:03 -0400

    Remove unused events/handlers from node-actions

--- addon/mixins/node-actions.js
+++ addon/mixins/node-actions.js
@@ -4,137 +4,14 @@ export default Ember.Mixin.create({
     node: null,
     model: null,
     _node: Ember.computed.or('node', 'model'),
-    _contributors: {},
 
-    /** Called when #updateNode succeeds
-     *
-     * @param {Node} node
+    /** Maps all node contributors to format {contribID: Contributor}
      **/
-    onUpdateNodeSuccess: Ember.on('updateNodeSuccess', function(/* node */) {}),
-    /** Called when  #updateNode fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     **/
-    onUpdateNodeFail: Ember.on('updateNodeFail', function(/* error, node */) {}),
-    /** Called when #deleteNode succeeds
-     **/
-    onDeleteNodeSuccess: Ember.on('deleteNodeSuccess', function() {}),
-    /** Called when #deleteNode fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     **/
-    onDeleteNodeFail: Ember.on('deleteNodeFail', function(/* error, node */) {}),
-    /** Called when #affiliateNode succeeds
-     *
-     * @param {Node} node
-     * @param {Institution} institution
-     **/
-    onAffiliateNodeSuccess: Ember.on('affiliateNodeSuccess', function(/* node, institution */) {}),
-    /** Called when #affiliateNode fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {Institution} institution
-     **/
-    onAffiliateNodeFail: Ember.on('affiliateNodeFail', function(/* error, node, institution */) {}),
-    /** Called when the institutionId specified in #affiliateNode can't be found
-     *
-     * @param {DS.Error} error
-     * @param {String} institutionId
-     **/
-    onInstitutionNotFound: Ember.on('institutionNotFound', function(/* error, institutionId */) {}),
-    /** Called when #unaffiliateNode succeeds
-     *
-     * @param {Node} node
-     * @param {Institution} institution
-     **/
-    onUnaffiliateNodeSuccess: Ember.on('unaffiliateNodeSuccess', function(/* node, institution */) {}),
-    /** Called when #unaffiliateNode fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {Institution} institution
-     **/
-    onUnaffiliateNodeFail: Ember.on('unaffiliateNodeFail', function(/* error, node, institution */) {}),
-    /** Called when #addContributor succeeds
-     *
-     * @param {Node} node
-     * @param {Contributor} contributor
-     **/
-    onAddContributorSuccess: Ember.on('addContributorSuccess', function(/* node, contributor */) {}),
-    /** Called when #addContributor fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {Contributor} contributor
-     **/
-    onAddContributorFail: Ember.on('addContributorFail', function(/* error, node, contributor */) {}),
-    /** Called when #removeContributor succeeds
-     *
-     * @param {Node} node
-     **/
-    onRemoveContributorSuccess: Ember.on('removeContributorSuccess', function(/* node */) {}),
-    /** Called when #removeContributor fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {Contributor} contributor
-     **/
-    onRemoveContributorFail: Ember.on('removeContributorFail', function(/* error, node, contributor */) {}),
-    /** Called when #updateContributors succeeds
-     *
-     * @param {Node} node
-     * @param {Contributor[]} contributors
-     **/
-    onUpdateContributorsSuccess: Ember.on('updateContributorsSuccess', function(/* node, contributors */) {}),
-    /** Called when #updateContributors fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {Contributor[]} contributors
-     **/
-    onUpdateContributorsFail: Ember.on('updateContributorsFail', function(/* error, node, contributors */) {}),
-    /** Called when #addChild succeeds
-     *
-     * @param {Node} node
-     * @param {Node} child
-     **/
-    onAddChildSuccess: Ember.on('addChildSuccess', function(/* node, child */) {}),
-    /** Called when #addChild fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {Node} child
-     **/
-    onAddChildFail: Ember.on('addChildFail', function(/* error, node, child */) {}),
-    /** Called when #addNodeLink succeeds
-     *
-     * @param {Node} node
-     * @param {NodeLink} nodeLink
-     **/
-    onAddNodeLinkSuccess: Ember.on('addNodeLinkSuccess', function(/* node, nodeLink*/) {}),
-    /** Called when #addNodeLink fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {NodeLink} nodeLink
-     **/
-    onAddNodeLinkFail: Ember.on('addNodeLinkFail', function(/* error, node, nodeLink */) {}),
-    /** Called when #removeNodeLink succeeds
-     *
-     * @param {Node} node
-     **/
-    onremoveNodeLinkSuccess: Ember.on('removeNodeLinkSuccess', function(/* node */) {}),
-    /** Called when #removeNodeLink fails
-     *
-     * @param {DS.Error} error
-     * @param {Node} node
-     * @param {NodeLink} nodeLink
-     **/
-    onRemoveNodeLinkFail: Ember.on('removeNodeLinkFail', function(/* error, node, nodeLink */) {}),
-
+    _generateContributorMap(contributors) {
+        var contribMap = {};
+        contributors.forEach(contrib => contribMap[contrib.id] = contrib);
+        return contribMap;
+    },
     actions: {
         updateNode(title, description, category, isPublic) {
             var node = this.get('_node');
@@ -150,34 +27,25 @@ export default Ember.Mixin.create({
             if (isPublic !== null) {
                 node.set('public', isPublic);
             }
-            node.save()
-                .then(() => this.send('updateNodeSuccess', node))
-                .catch(e => this.send('updateNodeFail', e, node));
+            return node.save();
         },
         deleteNode() {
             var node = this.get('_node');
-            node.destroyRecord()
-                .then(() => this.send('deleteNodeSuccess'))
-                .catch(e => this.send('deleteNodeFail', e, node));
+            return node.destroyRecord();
         },
         _affiliateNode(institution) {
             var node = this.get('_node');
             node.get('affiliatedInstitutions').pushObject(institution);
-            node.save()
-                .then(() => this.send('affiliateNodeSuccess', node, institution))
-                .catch(e => this.send('affiliateNodeFail', e, node, institution));
+            return node.save();
         },
         affiliateNode(institutionId) {
-            this.store.findRecord('institution', institutionId)
-                .then(institution => this._affiliateNode(institution))
-                .catch(e => this.send('institutionNotFound', e, institutionId));
+            return this.store.findRecord('institution', institutionId)
+                .then(institution => this._affiliateNode(institution));
         },
         unaffiliateNode(institution) {
             var node = this.get('_node');
             node.get('affiliatedInstitutions').removeObject(institution);
-            node.save()
-                .then(() => this.send('unaffiliateNodeSuccess', node, institution))
-                .catch(e => this.send('unaffiliateNodeFail', e, node, institution));
+            return node.save();
         },
         addContributor(userId, permission, isBibliographic) {
             var node = this.get('_node');
@@ -187,24 +55,14 @@ export default Ember.Mixin.create({
                 bibliographic: isBibliographic
             });
             node.get('contributors').pushObject(contributor);
-            node.save()
-                .then(() => this.send('addContributorSuccess', node, contributor))
-                .catch(e => this.send('addContributorFail', e, node, contributor));
+            return node.save();
         },
         removeContributor(contributor) {
             var node = this.get('_node');
             contributor.setProperties({
                 nodeId: node.id
             });
-            contributor.destroyRecord()
-                .then(() => this.send('removeContributorSuccess', node))
-                .catch(e => this.send('removeContributorFail', e, node, contributor));
-        },
-        _generateContributorMap(contributors) {
-            // Maps all node contributors to format {contribID: {permission: "read|write|admin", bibliographic: "true|false"}}
-            var contribMap = {};
-            contributors.forEach(contrib => contribMap[contrib.id] = contrib);
-            return contribMap;
+            return contributor.destroyRecord();
         },
         updateContributors(contributors, permissionsChanges, bibliographicChanges) {
             var node = this.modelFor(this.routeName);
@@ -219,9 +77,7 @@ export default Ember.Mixin.create({
                 }
                 contributorMap[contributorId].setProperties(props);
             }
-            node.save()
-                .then(() => this.sendAction('updateContributorsSuccess', node, contributors))
-                .catch(e => this.sendAction('updateContributorsFail', e, node, contributors));
+            return node.save();
         },
         addChild(title, description, category) {
             var node = this.get('_node');
@@ -231,9 +87,7 @@ export default Ember.Mixin.create({
                 description: description || null
             });
             node.get('children').pushObject(child);
-            node.save()
-                .then(() => this.sendAction('addChildSuccess', node, child))
-                .catch(e => this.sendAction('addChildFail', e, node, child));
+            return node.save();
         },
         addNodeLink(targetNodeId) {
             var node = this.get('_node');
@@ -241,15 +95,10 @@ export default Ember.Mixin.create({
                 target: targetNodeId
             });
             node.get('nodeLinks').pushObject(nodeLink);
-            node.save()
-                .then(() => this.send('addNodeLinkSuccess', node, nodeLink))
-                .catch(e => this.send('addNodeLinkFail', e, node, nodeLink));
+            return node.save();
         },
         removeNodeLink(nodeLink) {
-            var node = this.get('_node');
-            nodeLink.destroyRecord()
-                .then(() => this.send('removeNodeLinkSuccess', node))
-                .catch(e => this.send('removeNodeLinkFail', e, node, nodeLink));
+            return nodeLink.destroyRecord();
         }
     }
 });
