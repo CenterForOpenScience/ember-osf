627b80e16de33bb67a45a7ecd3a8d246a0e2b38b
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-14 10:36:44 -0400

    Get rid of ember-component-css
    - also add osf-style dev dependency

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,11 @@
 /*jshint node:true*/
 /* global require, module */
+var path = require('path');
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
     var app = new EmberAddon(defaults, {});
+
+    app.import(path.join(app.bowerDirectory, 'osf-style/css/base.css'));
     return app.toTree();
 };
