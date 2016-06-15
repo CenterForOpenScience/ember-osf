1f882f39513e56782b4a22df5dfeef8301723e6e
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-15 17:16:12 -0400

    Expand draft registration detail template to accomodate prereg schema.

--- tests/dummy/app/helpers/draft-text-input.js
+++ tests/dummy/app/helpers/draft-text-input.js
@@ -1,7 +1,20 @@
 import Ember from 'ember';
 
-export function draftTextInput([metadata, question]) {
-    var response = metadata[question.qid].value;
+export function draftTextInput([metadata, question, deepNest]) {
+    var response = '';
+    if (deepNest) {
+        if (metadata && metadata[question.qid]) {
+            response = metadata[question.qid].value.question.value;
+        }
+    } else {
+        if (metadata && metadata[question.qid]) {
+            if (Ember.typeOf(metadata[question.qid].value) === 'array') {
+                response = metadata[question.qid].value[0];
+            } else {
+                response = metadata[question.qid].value;
+            }
+        }
+    }
     return response;
 }
 
