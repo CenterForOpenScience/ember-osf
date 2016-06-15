babf85fa78cc2e748f13e11f69f220be4a01f144
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-15 13:55:44 -0400

    Create draft registration detail template for limited cases.

--- tests/dummy/app/routes/nodes/detail/draft-registrations/detail.js
+++ tests/dummy/app/routes/nodes/detail/draft-registrations/detail.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model(params) {
+        return this.store.peekRecord('draft-registration', params.draft_registration_id);
+    },
 });
