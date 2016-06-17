cb3ac962f8f6e2ff0ab771bc99896971ff04e104
Andy Boughton <abought@gmail.com>
2016-06-17 16:26:41 -0400

    Notes / testing plan
    [ci skip]
    [#EOSF-70]

--- tests/unit/mixins/fetch-all-route-test.js
+++ tests/unit/mixins/fetch-all-route-test.js
@@ -10,3 +10,10 @@ test('it works', function(assert) {
     let subject = FetchAllObject.create();
     assert.ok(subject);
 });
+
+
+// TODO
+// Things I'd like to test...
+// 1. Mock the fetch operation. Confirm that if we expect 3 pages of results, then a function will fire 3x by end of test (will need to block test execution until done?)
+// 2. Confirm the modelPath variable contains the expected number of results when done
+// 3. Test configuration of relationship fetching works as expected
