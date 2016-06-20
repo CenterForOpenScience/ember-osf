6cce9a4c271265f15e7a52448166a31980f686d8
Andy Boughton <abought@gmail.com>
2016-06-20 17:15:18 -0400

    Cleanup in progress
    [ci skip]

--- tests/unit/mixins/fetch-all-route-test.js
+++ tests/unit/mixins/fetch-all-route-test.js
@@ -1,19 +1,18 @@
 import Ember from 'ember';
 import { module, test } from 'qunit';
-import sinonTest from 'dummy/tests/ember-sinon-qunit/test';
 
 import FetchAllRouteMixin from 'ember-osf/mixins/fetch-all-route';
 
-module('Unit | Mixin | fetch all route' //, {
+module('Unit | Mixin | fetch all route', {
 
     // TODO: Once tests working, refactor to use before and beforeEach
-    //
-    // before() {
-    //     this.routeClass =
-    // },
-    // beforeEach() {
-    //
-    // }}
+
+    before() {
+        this.routeClass = 1;
+    },
+    beforeEach() {
+
+    }}
 );
 
 
@@ -28,6 +27,17 @@ function callModelHook(route) {
     });
 }
 
+function mockQuery() {
+    return Ember.RSVP.resolve(
+        Ember.ArrayProxy.create({
+            content: [1, 2, 3, 4],
+            meta: {
+                total: 3
+            }
+        })
+    );
+}
+
 // Replace this with your real tests.
 test('it works', function(assert) {
     let FetchAllObject = Ember.Object.extend(FetchAllRouteMixin);
@@ -36,38 +46,28 @@ test('it works', function(assert) {
 });
 
 
-sinonTest('Auto-fetches multiple pages of results when expected', function(assert) {
+test('Auto-fetches multiple pages of results when expected', function(assert) {
 
     let FetchAllRoute = Ember.Route.extend(FetchAllRouteMixin, {
-        fakeStorage: Ember.A([1,2,3,4]), // infinity doesn't start pushing objects until page 2, so mimic page 1 being in storage
+        fakeStorage: null,
 
         model() {
             return this.infinityModel('some_model', {
                 modelPath: 'fakeStorage',
-                _storeFindMethod: function() {
-
-                    return Ember.RSVP.resolve(
-                        Ember.ArrayProxy.create({
-                            content: [1, 2, 3, 4],
-                            meta: {
-                                total: 3
-                            }
-                    }));
-                }
+                _storeFindMethod: mockQuery
             });
         },
 
         infinityModelLoaded({totalPages}) {
-            console.log('get assertive', ...arguments);
             let storage = this.get('fakeStorage');
-            console.log(storage);
-
             assert.equal(totalPages, 3);
             assert.equal(storage.length, 12);
         }
     });
 
-    let subject = FetchAllRoute.create();
+    let subject = FetchAllRoute.create({
+        'fakeStorage': Ember.A([1,2,3,4])  // infinity doesn't start pushing objects until page 2, so prefill storage with page 1
+    });
     callModelHook(subject);
 });
 
@@ -75,7 +75,7 @@ sinonTest('Auto-fetches multiple pages of results when expected', function(asser
 
 // TODO
 // Things I'd like to test...
-// 1. Mock the fetch operation. Confirm that if we expect 3 pages of results, then a function will fire 3x by end of test (will need to block test execution until done?)
-// 2. Confirm the modelPath variable contains the expected number of results when done
+// 1.[x] Mock the fetch operation. Confirm that if we expect 3 pages of results, then a function will fire 3x by end of test (will need to block test execution until done?)
+// 2. [x] Confirm the modelPath variable contains the expected number of results when done
 // 3. Test configuration of relationship fetching works as expected
 
