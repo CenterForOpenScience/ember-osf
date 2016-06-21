9345d99c5ef7b4f0b603421cd8433de942b2cc7c
Sam Chrisinger <s.chrisinger@gmail.com>
2016-06-21 17:04:56 -0400

    Add component for OAuth login without redirection
    It opens a new tab and automatically closes that tab when the OAuth flow
    is complete

---
+++ addon/components/oauth-popup/component.js
@@ -0,0 +1,63 @@
+import Ember from 'ember';
+import layout from './template';
+
+import { getTokenFromHash } from 'ember-osf/utils/auth';
+
+export default Ember.Component.extend({
+    layout,
+    session: Ember.inject.service(),
+    popupHeight: 550,
+    popupWidth: 500,
+    authUrl: null,
+    popup: null,
+    redirectRoute: null,
+    _openPopup() {
+        let {
+            popupHeight,
+            popupWidth,
+            popup
+        } = this.getProperties('popupWidth', 'popupHeight', 'popup');
+
+        if (popup) {
+            popup.close();
+        }
+        let width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        let height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        let left = ((width / 2) - (popupWidth / 2));
+        let top = (height / 2);
+        this.set('popup', window.open(
+            this.get('authUrl'),
+            'Authentication',
+            `resizeable=true, width=${popupWidth}, height=${popupHeight}, top=${top}, left=${left}`
+        ));
+        // Puts focus on the new window
+        if (window.focus) {
+            this.popup.focus();
+        }
+
+        // wait for the popup window to be closed, by the user or completion of authorization redirect.
+        return new Ember.RSVP.Promise((resolve /* , reject */ ) => {
+            let timer = window.setInterval(() => {
+		popup = this.get('popup');		
+		if (!popup || (popup.location.origin === window.location.origin)) {
+		    var accessToken = getTokenFromHash(popup.location.hash);
+		    popup.close();
+                    window.clearInterval(timer);
+                    resolve(accessToken);
+                }
+            }, 100);
+        });
+    },
+    actions: {
+	login() {
+	    this._openPopup().then((accessToken) => {
+		return this.get('session').authenticate('authenticator:osf-token', accessToken)
+		    .then(
+			() => this.sendAction('loginSuccess'),
+			err => this.sendAction('loginFail', err)
+		    );
+	    });
+	}
+    }
+});
---
+++ tests/unit/utils/auth-test.js
@@ -0,0 +1,10 @@
+import auth from 'dummy/utils/auth';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | auth');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = auth();
+  assert.ok(result);
+});
