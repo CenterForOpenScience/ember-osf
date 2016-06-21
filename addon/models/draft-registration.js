85477d35feecfa729b3eeb22c18f5d99a1e61ec6
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-21 11:12:26 -0400

    Add documentation comments to draft-registration and metaschema models.

--- addon/models/draft-registration.js
+++ addon/models/draft-registration.js
@@ -1,12 +1,22 @@
 import DS from 'ember-data';
 import OsfModel from './osf-model';
 
+/**
+ * Model for OSF APIv2 draft registrations.
+ * This model represents draft registration data - the initiator of the draft,
+ * the registrationSchema for the draft, and the registrationMetadata field, which is an object
+ * containing responses to the supplemental questions described in the
+ * registrationSchema.  branchedFrom is the node the user wants to register, and
+ * registrationSupplement is the id of the registrationSchema (used on draft registration creation.)
+ * For information on how to interact with a node's draft registrations, see:
+ *    https://api.osf.io/v2/docs/#!/v2/Node_Draft_Registrations_List_GET
+ *    https://api.osf.io/v2/docs/#!/v2/Node_Draft_Registration_Detail_GET
+*/
 export default OsfModel.extend({
     registrationSupplement: DS.attr('string'),
     registrationMetadata: DS.attr(),
     datetimeInitiated: DS.attr('date'),
     datetimeUpdated: DS.attr('date'),
-    requiresApproval: DS.attr('boolean'),
     branchedFrom: DS.belongsTo('node', {
         inverse: null
     }),
--- addon/models/metaschema.js
+++ addon/models/metaschema.js
@@ -1,6 +1,15 @@
 import DS from 'ember-data';
 
 import OsfModel from './osf-model';
+/**
+ * Model for OSF APIv2 metaschemas.
+ * This model describes metaschemas, which contain the
+ * supplemental questions accompanying a registration.  The schema field itself
+ * contains the detailed blueprint of the metaschema.
+ * For information on how to retrieve metaschemas see:
+ *    https://api.osf.io/v2/docs/#!/v2/Meta_Schemas_List_GET
+ *    https://api.osf.io/v2/docs/#!/v2/Meta_Schema_Detail_GET
+*/
 
 export default OsfModel.extend({
     name: DS.attr('string'),
