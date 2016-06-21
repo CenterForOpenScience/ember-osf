3a799f7a2b43a5dd7d39b06b4f7a6bd190c6a810
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-21 14:23:34 -0400

    Avoid duplication of field name description on models.

--- addon/models/metaschema.js
+++ addon/models/metaschema.js
@@ -3,9 +3,7 @@ import DS from 'ember-data';
 import OsfModel from './osf-model';
 /**
  * Model for OSF APIv2 metaschemas.
- * This model describes metaschemas, which contain the
- * supplemental questions accompanying a registration.  The schema field itself
- * contains the detailed blueprint of the metaschema.
+ * This model describes metaschemas and can be directly queried.
  * For information on how to retrieve metaschemas see:
  *    https://api.osf.io/v2/docs/#!/v2/Meta_Schemas_List_GET
  *    https://api.osf.io/v2/docs/#!/v2/Meta_Schema_Detail_GET
