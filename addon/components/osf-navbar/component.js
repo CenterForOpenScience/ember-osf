e7bd503907f22f893452b69483088f6313349865
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-23 15:34:16 -0400

    Add gravatarUrl and fullName to component.

--- addon/components/osf-navbar/component.js
+++ addon/components/osf-navbar/component.js
@@ -5,6 +5,8 @@ export default Ember.Component.extend({
     layout,
     session: Ember.inject.service('session'),
     onSearchPage: false,
+    gravatarUrl: null,
+    fullName: null,
 
     // TODO: Make these parameters configurable from... somewhere. (currently set by OSF settings module)
     allowLogin: true,
