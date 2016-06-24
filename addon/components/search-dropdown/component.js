f40856899172a7cf7186b39023a81b7cb26e36b2
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-24 13:28:11 -0400

    Search when arrow clicked.

--- addon/components/search-dropdown/component.js
+++ addon/components/search-dropdown/component.js
@@ -1,12 +1,20 @@
 import Ember from 'ember';
 import layout from './template';
+import config from 'ember-get-config';
 
 export default Ember.Component.extend({
     layout,
+    host: config.OSF.url,
     actions: {
         // Runs toggleSearch in parent component, osf-navbar
         toggleSearch() {
             this.sendAction('action');
+        },
+        search() {
+            var query = Ember.$(searchPageFullBar)[0].value;
+            if (query) {
+                window.location.href = this.host + 'search/?q=' + query;
+            }
         }
     }
 });
