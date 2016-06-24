b833e3234ce9cf7cd6e9f9ec21a9eff9db4de6e1
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-06-24 10:38:23 -0400

    Get search dropdown to toggle on and off.

--- addon/components/search-dropdown/component.js
+++ addon/components/search-dropdown/component.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 import layout from './template';
 
 export default Ember.Component.extend({
-    layout
+    layout,
+    actions: {
+        // Runs toggleSearch in parent component, osf-navbar
+        toggleSearch() {
+            this.sendAction('action');
+        }
+    }
 });
