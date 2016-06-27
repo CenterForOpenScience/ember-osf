1587ba4a99e833551b7544189a3a269e6e33d46b
Abram Booth <abram@cos.io>
2016-06-27 15:40:10 -0400

    Don't wipe out item state when new items are added

--- addon/components/file-browser/component.js
+++ addon/components/file-browser/component.js
@@ -6,7 +6,7 @@ import loadAll from 'ember-osf/utils/load-relationship';
 /*
  * Wrapper for file items. Includes state for the item's row.
  */
-let FileItem = Ember.ObjectProxy.extend({
+let ItemWrapper = Ember.ObjectProxy.extend({
     isSelected: false,
 
     // TODO (Abram) update childItems when `children` or `files` changes
@@ -16,6 +16,7 @@ let FileItem = Ember.ObjectProxy.extend({
         let children = this._setupLoadAll('children', '_children', '_childrenLoaded');
 
         let wrappedItems = Ember.A();
+        let wrapItem = this.get('_wrapItem').bind(this);
         if (files) {
             wrappedItems.addObjects(files.map(wrapItem));
         }
@@ -26,10 +27,6 @@ let FileItem = Ember.ObjectProxy.extend({
     }),
     _files: null,
     _children: null,
-
-    childItemsLoaded: Ember.computed.and('_filesLoaded', '_childrenLoaded'),
-    _filesLoaded: false,
-    _childrenLoaded: false,
 
     _setupLoadAll(relationship, destName, loaded) {
         let dest = this.get(destName);
@@ -45,24 +42,35 @@ let FileItem = Ember.ObjectProxy.extend({
             }
         }
         return dest;
+    },
+
+    childItemsLoaded: Ember.computed.and('_filesLoaded', '_childrenLoaded'),
+    _filesLoaded: false,
+    _childrenLoaded: false,
+
+    unwrap() {
+        return this.get('content');
+    },
+    _wrappedItems: {},
+    _wrapItem(item) {
+        if (item instanceof ItemWrapper) {
+            return item;
+        }
+        let id = item.get('id');
+        let wrappedItems = this.get('_wrappedItems');
+        if (id in wrappedItems) {
+            let wrapped = wrappedItems[id];
+            wrapped.set('content', item);
+            return wrapped;
+        }
+
+        let wrapped = ItemWrapper.create({
+            content: item
+        });
+        wrappedItems[item.get('id')] = wrapped;
+        return wrapped;
     }
 });
-
-function wrapItem(item) {
-    if (item instanceof FileItem) {
-        return item;
-    }
-    return FileItem.create({
-        content: item
-    });
-}
-
-function unwrapItem(item) {
-    if (item instanceof FileItem) {
-        return item.get('content');
-    }
-    return item;
-}
 
 /*
  * {{file-browser rootItem=item openFile=(action 'openFile') openNode=(action 'openNode')}}
@@ -79,7 +87,9 @@ export default Ember.Component.extend({
             return this.get('breadcrumbs.firstObject');
         },
         set(_, item) {
-            let wrappedItem = wrapItem(item);
+            let wrappedItem = ItemWrapper.create({
+                content: item
+            });
             this.set('breadcrumbs', Ember.A([wrappedItem]));
         }
     }),
@@ -89,6 +99,7 @@ export default Ember.Component.extend({
     itemsLoaded: Ember.computed.readOnly('currentParent.childItemsLoaded'),
     selectedItems: Ember.computed.filterBy('items', 'isSelected', true),
 
+    // TODO better way of setting/updating itemWidth
     loadedChanged: Ember.observer('itemsLoaded', function() {
         let containerWidth = this.$().width();
         this.set('itemWidth', containerWidth);
@@ -98,19 +109,19 @@ export default Ember.Component.extend({
         selectItem(item) {
             item.set('isSelected', true);
             if (item.get('isFile') && this.get('selectFile')) {
-                this.sendAction('selectFile', unwrapItem(item));
+                this.sendAction('selectFile', item.unwrap());
             }
             if (item.get('isNode') && this.get('selectNode')) {
-                this.sendAction('selectNode', unwrapItem(item));
+                this.sendAction('selectNode', item.unwrap());
             }
         },
 
         openItem(item) {
             if (item.get('isFile') && this.get('openFile')) {
-                this.sendAction('openFile', unwrapItem(item));
+                this.sendAction('openFile', item.unwrap());
             }
             if (item.get('isNode') && this.get('openNode')) {
-                this.sendAction('openNode', unwrapItem(item));
+                this.sendAction('openNode', item.unwrap());
             }
             if (item.get('canHaveChildren')) {
                 this.send('navigateToItem', item);
@@ -121,22 +132,16 @@ export default Ember.Component.extend({
             let breadcrumbs = this.get('breadcrumbs');
             let index = breadcrumbs.indexOf(item);
             if (index === -1) {
-                // TODO: Valid to assume item is a child of currentParent?
+                // TODO (Abram) Valid to assume item is a child of currentParent?
                 breadcrumbs.pushObject(item);
             } else {
                 let slicedBread = breadcrumbs.slice(0, index + 1);
                 this.set('breadcrumbs', Ember.A(slicedBread));
             }
-        },
 
-        navigateUp() {
-            let breadcrumbs = this.get('breadcrumbs');
-            if (breadcrumbs.length === 1) {
-                return;
-            }
-            breadcrumbs.popObject();
+            let newPath = breadcrumbs.mapBy('itemName').join('/');
+            this.set('path', newPath);
+            this.sendAction('pathChanged', newPath);
         }
     }
 });
-
-
