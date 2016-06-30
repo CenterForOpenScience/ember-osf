8fcb384edf2817c3799931fc807680b63c5b5310
Andy Boughton <abought@gmail.com>
2016-06-29 22:49:25 -0400

    Update for merge conflicts.
    [ci skip]

--- addon/adapters/osf-adapter.js
+++ addon/adapters/osf-adapter.js
@@ -6,7 +6,7 @@ import DS from 'ember-data';
 
 import HasManyQuery from 'ember-data-has-many-query';
 import config from 'ember-get-config';
-import GenericDataAdapterMixin from 'ember-osf/mixins/generic-data-adapter';
+import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 
 let inflector = new Ember.Inflector(Ember.Inflector.defaultRules);
