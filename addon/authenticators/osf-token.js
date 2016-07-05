fa19d7c7f30248012cd89a95df41b793f59bff0e
Andy Boughton <abought@gmail.com>
2016-07-05 17:52:06 -0400

    Fix wording

--- addon/authenticators/osf-token.js
+++ addon/authenticators/osf-token.js
@@ -11,7 +11,7 @@ import config from 'ember-get-config';
 /**
  * Ember-simple-auth compatible authenticator based on OAuth2 bearer tokens.
  *
- * Intended to be used with the authenticator of the same name.
+ * Intended to be used with the authorizer of the same name.
  *
  * @class OsfTokenAuthenticator
  * @extends ember-simple-auth/BaseAuthenticator
