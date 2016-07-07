4030a9f8d5edfefe95bbf8b7b4b08d4a9d7d9029
Lauren Revere <geeksnglitter@gmail.com>
2016-07-07 12:16:25 -0400

    Add waterbutler url for test server configuration

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ module.exports = {
             ENV.OSF.apiUrl = 'https://test-api.osf.io';
             ENV.OSF.oauthUrl = 'https://test-accounts.osf.io/oauth2/authorize';
             ENV.OSF.renderUrl = 'http://test-mfr.osf.io/render';
-            ENV.OSF.waterbutlerUrl = 'https://test-files.osf.io/';
+            ENV.OSF.waterbutlerUrl = 'http://test-files.osf.io/';
 
         }
         if (BACKEND === 'prod') {
