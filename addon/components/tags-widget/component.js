f8030ae572404c049f31a73c10f59c82690df213
Andy Boughton <abought@gmail.com>
2016-07-07 13:09:31 -0400

    Shoehorn jquery-tags-input widget + styles into component.
     TODO: Add config, verify server requests sent, and add more styling.
    
    [ci skip]

--- addon/components/tags-widget/component.js
+++ addon/components/tags-widget/component.js
@@ -9,6 +9,9 @@ import layout from './template';
 /**
  * Allow the user to view and manage tags.
  * See TaggableMixin for controller actions that can be used with this component.
+ *
+ * For more information on configuration options, see documentation for [jquery-tags-input](https://github.com/xoxco/jQuery-Tags-Input).
+ *
  * ```handlebars
  * {{tags-widget
  *   addATag=(action 'addATag')
@@ -21,6 +24,18 @@ export default Ember.Component.extend({
     layout,
     newTag: null,
     tags: [],
+
+    tagName : "input",
+    type : "text",
+    attributeBindings : [ "name", "type", "value", "id"],
+
+
+    _initialize: Ember.on('didInsertElement', function () {
+        Ember.run.scheduleOnce('afterRender', this, function() {
+            this.$().tagsInput({'width': '100px'});
+        });
+    }),
+
     actions: {
         addATag(tag) {
             let res = this.attrs.addATag(tag);
