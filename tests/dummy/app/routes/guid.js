63d47c61032f9c39027aca8e40d168ec7c66e02e
Sam Chrisinger <s.chrisinger@gmail.com>
2016-07-19 15:26:15 -0400

    Add place to test guid route behavior

--- tests/dummy/app/routes/guid.js
+++ tests/dummy/app/routes/guid.js
@@ -4,23 +4,5 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
     model(params) {
         return this.store.findRecord('guid', params.guid).then(g => g.get('referent'));
-    },
-    renderTemplate(controller, model) {
-        let kind = model.constructor.modelName;
-        if (kind === 'node') {
-            this.render('nodes.detail.index', {
-                model: model
-            });
-        } else if (kind === 'user') {
-            this.render('users.detail', {
-                model: model
-            });
-        } else if (kind === 'file') {
-            this.render('files.detail', {
-                model: model
-            });
-        } else {
-            // 404 page? This should be impossible
-        }
     }
 });
