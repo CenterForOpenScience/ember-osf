990e52dc9cfe697d9270e67671cafccbdef63afd
Andy Boughton <abought@gmail.com>
2016-07-20 10:56:38 -0400

    Parity for buttons displayed on comments
     [ci skip]
    [#EOSF-100]

--- addon/components/comment-detail/component.js
+++ addon/components/comment-detail/component.js
@@ -22,6 +22,7 @@ import layout from './template';
  * ```
  * @class comment-detail
  * @param {DS.Model} comment The comment to display
+ * @param {DS.Model} resource The parent resource that owns the comment
  * @param {action} editComment
  * @param {action} deleteComment
  * @param {action} restoreComment
@@ -29,6 +30,10 @@ import layout from './template';
  */
 export default Ember.Component.extend({
     layout,
+    /**
+     * The comment to display
+     * @property comment
+     */
     comment: null,
     /**
      * The parent resource to which comments are attached.
@@ -36,6 +41,9 @@ export default Ember.Component.extend({
      * @type Node|File|Wiki|Comment
      */
     resource: null, // TODO: Track whether the node is being viewed in "anonymous" mode and change how authors are displayed
+
+    // Store information about direct replies to this comment
+    _replies: Ember.A(),
 
     // Conditionals that control display of template sections
     replyMode: false,
@@ -47,7 +55,7 @@ export default Ember.Component.extend({
 
     // Conditionals that track actions in progress
 
-
+    // TODO: Perhaps these fields should be defined on the comment?
     isDeletedAbuse: Ember.computed('comment.deleted', 'comment.isAbuse', function() {
         return this.get('comment.deleted') && this.get('comment.isAbuse');
     }),
@@ -57,11 +65,12 @@ export default Ember.Component.extend({
     isAbuseNotDeleted: Ember.computed('comment.deleted', 'comment.isAbuse', function() {
         return !this.get('comment.deleted') && this.get('comment.isAbuse');
     }),
-
-    isVisible: Ember.computed('comment.deleted', 'comment.isAbuse', function() {
+    // TODO: Rename this variable after parity with source is achieved
+    // Whether to show comment text. Source template called "isVisible", which has special meaning in ember....!
+    isShowingContent: Ember.computed('comment.deleted', 'comment.isAbuse', function() {
         return !this.get('comment.deleted') && !this.get('comment.isAbuse');
     }),
-    
+
     canComment: Ember.computed.alias('resource.currentUserCanComment'),
     canReport: Ember.computed('canComment', 'comment.canEdit', function() {
         return this.get('canComment') && !this.get('comment.canEdit');
@@ -70,8 +79,11 @@ export default Ember.Component.extend({
     actions: {
         toggleChildren() {
             // TODO: Implement toggling of children widgets
+            if (!this.get('showChildren')) {
+                this.send('fetchChildren');
+            }
+            // TODO: If fetch is in progress, should there be a loading indicator of some sort?
             this.toggleProperty('showChildren');
-            // TODO: Fetch children as needed
         },
         toggleEditMode() {
             // Allow editing of a pre-existing comment
@@ -85,9 +97,19 @@ export default Ember.Component.extend({
             // Show buttons relevant to comment deletion
             this.toggleProperty('deleteMode');
         },
-        reportMode() {
+        toggleReportMode() {
             // Show buttons relevant to reporting a comment
             this.toggleProperty('reportMode');
+        },
+
+        fetchChildren() {
+            // Fetch child comments (replies)
+
+            // TODO:
+            // 1. track what page of results to fetch
+            // 2. Handle additional query params, embeds, viewonly behaviors, related counts etc
+            // 3. Ensure that children are rendered.
+            // 4. Ensure that when a reply is added, it's added to the list of known children.
         }
     }
 });
