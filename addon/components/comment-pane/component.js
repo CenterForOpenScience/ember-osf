bb1e6cff12e353a4985ea7534ceadd95a4cf1267
Andy Boughton <abought@gmail.com>
2016-07-19 22:56:26 -0400

    Pass resource into sample templates
     [ci skip]
    [#EOSF-100]

--- addon/components/comment-pane/component.js
+++ addon/components/comment-pane/component.js
@@ -14,6 +14,7 @@ import layout from './template';
  * ```handlebars
  * {{comment-pane
  *   comments=comments
+ *   resource=model
  *   addComment=(action 'addComment')
  *   editComment=(action 'editComment')
  *   deleteComment=(action 'deleteComment')
