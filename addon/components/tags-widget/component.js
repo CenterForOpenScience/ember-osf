66b8a3ee221f7a886714fec184b02bf38dadda86
Ellis Tsung <el1t@ymail.com>
2016-07-21 10:24:51 -0400

    Convert `tags-widget` to ember
    Removes jquery.tagsinput dependency as well as typeahead.js (previously unused)

--- addon/components/tags-widget/component.js
+++ addon/components/tags-widget/component.js
@@ -9,62 +9,83 @@ import layout from './template';
 /**
  * Allow the user to view and manage tags.
  * See TaggableMixin for controller actions that can be used with this component.
- *
- * For more information on configuration options, see documentation for [jquery-tags-input](https://github.com/xoxco/jQuery-Tags-Input).
+ * Pattern is a regular expression to validate input against.
  *
  * ```handlebars
  * {{tags-widget
- *   addATag=(action 'addATag')
- *   removeATag=(action 'removeATag')
- *   tags=model.tags}}
+ *   addTag=(action 'addTag')
+ *   removeTag=(action 'removeTag')
+ *   tags=model.tags
+ *   pattern='^[\\w-]+$'}}
  * ```
  * @class tags-widget
  * @extends Ember.Component
  */
 export default Ember.Component.extend({
     layout,
-
-    /**
-     * Whether the user is allowed to edit tags.
-     * @property canEdit
-     * @type {Boolean}
-     */
-    canEdit: true,  // TODO: Implement editing decision logic in the future
+    classNameBindings: ['invalid:has-error'],
+    pattern: /^.{1,128}$/,
     tags: [],
-
-    tagName: 'input',
-    type: 'text',
-    attributeBindings: ['name', 'type', 'value', 'id'],
-
-    _initialize: Ember.on('didInsertElement', function () {
-        Ember.run.scheduleOnce('afterRender', this, function() {
-            this.$().tagsInput({
-                width: '100%',
-                interactive: this.get('canEdit'),
-                maxChars: 128,
-                onAddTag: (tag) => {
-                    this.send('addATag', tag);
-                },
-                onRemoveTag: (tag) => {
-                    this.send('removeATag', tag);
+    value: '',
+    didReceiveAttrs() {
+        this._super(...arguments);
+        // Initialize attributes
+        const tags = this.get('tags');
+        const pattern = this.get('pattern');
+        if (!Ember.isArray(tags)) {
+            this.set('tags', tags ? [tags] : []);
+        }
+        if (typeof pattern === 'string') {
+            this.set('pattern', new RegExp(pattern));
+        }
+    },
+    keyDown(evt) {
+        const value = this.get('value');
+        switch (evt.keyCode) {
+            case 13: // Enter
+                // Add tag to list after validating
+                const text = Ember.$.trim(value).replace(/\s{2,}/g, ' ');
+                if (text && this.get('pattern').test(text) && !this.get('tags').find(tag => tag === text)) {
+                    this.send('addTag', text);
                 }
-            });
-            // Populate the widget with the default tags passed in
-            let tags = this.get('tags');
-            this.$().importTags(tags.join(', '));
-        });
-    }),
-
+                break;
+            case 8: // Backspace
+                // Pop last tag back into input
+                if (!value && this.get('tags').length) {
+                    const tag = this.get('tags').popObject();
+                    this.set('value', `${tag} `);
+                    this.sendAction('removeTag', tag);
+                }
+                break;
+        }
+    },
+    keyUp() {
+        // Manual form validation
+        const text = Ember.$.trim(this.get('value')).replace(/\s{2,}/g, ' ');
+        this.set('invalid', text && !this.get('pattern').test(text));
+    },
+    click() {
+        this.set('focused', true);
+        this.$('input').focus();
+    },
+    // Bubbles from input
+    focusOut() {
+        this.set('focused', false);
+    },
+    // Bubbles from input, handles alternate methods of focusing on input
+    focusIn() {
+        this.set('focused', true);
+    },
     actions: {
-        addATag(tag) {
-            this.attrs.addATag(tag);
+        addTag(tag) {
+            this.get('tags').pushObject(tag);
+            this.set('value', '');
+            this.sendAction('addTag', tag);
         },
-        removeATag(tag) {
-            // Don't try to delete a blank tag (would result in a server error)
-            if (!tag) {
-                return false;
-            }
-            this.sendAction('removeATag', tag);
+        removeAt(index) {
+            const tag = this.get('tags').objectAt(index);
+            this.get('tags').removeAt(index);
+            this.sendAction('removeTag', tag);
         }
     }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -31,8 +31,6 @@ module.exports = function(defaults) {
     app.import(path.join(app.bowerDirectory, 'dropzone/dist/dropzone.css'));
     app.import(path.join(app.bowerDirectory, 'dropzone/dist/dropzone.js'));
 
-    app.import(path.join(app.bowerDirectory, 'jquery.tagsinput/src/jquery.tagsinput.js'));
-
     app.import(path.join(app.bowerDirectory, 'osf-style/css/base.css'));
     app.import('vendor/assets/ember-osf.css');
     return app.toTree();
