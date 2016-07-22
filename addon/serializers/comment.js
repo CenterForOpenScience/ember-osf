2ebe47888b906543a553e57b56229202dc4c3f03
Andy Boughton <abought@gmail.com>
2016-07-22 11:07:48 -0400

    Save some cleanup for relationship checking- doesn't work pending createRelated changes
    Stashing wip to shift to contribs page
    
     [ci skip]
    [#EOSF-100]

--- addon/serializers/comment.js
+++ addon/serializers/comment.js
@@ -2,28 +2,26 @@ import Ember from 'ember';
 import OsfSerializer from './osf-serializer';
 
 export default OsfSerializer.extend({
-    serialize(snapshot, options) {  // jshint ignore:line
-        // Add relationships field to identify comment target
-        let serialized = this._super(...arguments);
+    serializeIntoHash(hash, typeClass, snapshot, options) {  // jshint ignore:line
+        if (options.forRelationship) {
+            // New comments must identify their target as part of a relationship field
+            // TODO: pop off record when done so these dummy fields don't persist? Do they matter?
+            // TODO: This breaks commenting atm
+            let targetID = snapshot.record.get('targetID');
+            let targetType = snapshot.record.get('targetType');
+            Ember.assert('Must provide target ID', targetID);
+            Ember.assert('Must provide target type', targetType);
 
-        // For POST requests specifically, two additional fields are needed
-        let targetID = snapshot.record.get('targetID');
-        let targetType = snapshot.record.get('targetType');
-        // TODO: Find a better way to detect request type, so this can enforce fields that are needed for creation (but not for editing)
-        //Ember.assert('Must provide target ID', targetID);
-        //Ember.assert('Must provide target type', targetType);
-
-        if (targetID && targetType) {
-            serialized.data.relationships = {
+            hash.data.relationships = {
                 target: {
                     data: {
                         id: targetID,
                         type: targetType
                     }
                 }
-            };
+            }
         }
-        return serialized;
+        return this._super(hash, typeClass, snapshot, options);
     },
     extractRelationships(modelClass, resourceHash) {
         // TODO: remove when https://openscience.atlassian.net/browse/OSF-6646 is done
