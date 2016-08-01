bc5af30ccab827575073b1239cf3b31bcd022317
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-08-01 13:57:03 -0400

    Add temporary preprint-form-authors component - will be moved to preprint repo once everything is hooked up.

---
+++ tests/dummy/app/components/preprint-form-authors/component.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import layout from './template';
+
+import permissions from 'ember-osf/const/permissions';
+
+export default Ember.Component.extend({
+    READ: permissions.READ,
+    WRITE: permissions.WRITE,
+    ADMIN: permissions.ADMIN,
+    layout: layout,
+    permissionChanges: {},
+    bibliographicChanges: {},
+    actions: {
+        addContributor(userId, permission, isBibliographic) {
+            this.sendAction('addContributor', userId, permission, isBibliographic);
+        },
+        removeContributor(contrib) {
+            this.sendAction('removeContributor', contrib);
+        },
+        permissionChange(contributor, permission) {
+            this.set(`permissionChanges.${contributor.id}`, permission.toLowerCase());
+        },
+        bibliographicChange(contributor, isBibliographic) {
+            this.set(`bibliographicChanges.${contributor.id}`, isBibliographic);
+        },
+        updateContributors() {
+            this.sendAction(
+                'editContributors',
+                this.get('contributors'),
+                this.get('permissionChanges'),
+                this.get('bibliographicChanges')
+            );
+        }
+    }
+});
