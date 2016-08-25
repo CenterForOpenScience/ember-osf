f544da905c718d0bad9d29cc521ba84ef905d69e
Andy Boughton <abought@gmail.com>
2016-08-25 18:03:00 -0400

    fix logout button. Smidge of a hack, possible race condition.

--- addon/authenticators/osf-cookie.js
+++ addon/authenticators/osf-cookie.js
@@ -41,14 +41,17 @@ export default Base.extend({
      * @method invalidate
      */
     invalidate(data) {
-        // If the user is logged in and data is not a response with the status unauthorized
-        // Actually log them out
-        if (data.id && data.status !== 401) {
+        if (!data || data.id && data.status !== 401) {
+            // If invalidate is called when loading the page to check if a cookie has permissions, don't redirect the user
+            // But if invalidate is called without arguments, or for any other reason, interpret this as a straight logout request
+            //   (and let the session get invalidated next time at the start of first page load)
             // Can't do this via AJAX request because it redirects to CAS, and AJAX + redirect = CORS issue
             window.location = `${config.OSF.url}logout/`;
+        } else {
+            // This branch is expected to be called when a test request reveals the user to lack permissions... so session should be wiped
+            return Ember.RSVP.resolve();
         }
-
-        return Ember.RSVP.resolve();
+        // FIXME: Possible race condition where returning resolved promise after window.location change calls logout to fail
     },
     /**
      * For now, simply verify that a token is present and can be used
