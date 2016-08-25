15ad0630cac4871cec9731d2972c51d25e25ac23
erinspace <erin.braswell@gmail.com>
2016-08-25 16:05:31 -0400

    get the sendEmail parameter from the snapshot about to be saved in the contributor adapter

--- addon/adapters/contributor.js
+++ addon/adapters/contributor.js
@@ -4,8 +4,10 @@ export default OsfAdapter.extend({
     buildURL(modelName, id, snapshot, requestType) { // jshint ignore:line
         if (requestType === 'createRecord' || requestType === 'findRecord') {
             var nodeId;
+            var sendEmail = true;
             if (snapshot) {
                 nodeId = snapshot.record.get('nodeId');
+                sendEmail = snapshot.record.get('sendEmail')
             } else {
                 nodeId = id.split('-').shift();
             }
@@ -21,8 +23,14 @@ export default OsfAdapter.extend({
                     return `${base}${id.split('-').pop()}/`;
                 }
 
+                var requestUrl = `${base}?embed=node`;
+
+                if (!sendEmail) {
+                    requestUrl += `&send_email=false`;
+                }
+
                 // Needed for Ember Data to update the inverse record's (the node's) relationship
-                return `${base}?embed=node`;
+                return requestUrl;
             } else {
                 throw new Error('Trying to add a contributor to a Node that hasn\'t been loaded into the store');
             }
