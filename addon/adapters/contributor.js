96cecc001e9d4647f8b83dc743ea8bc023a0f787
erinspace <erin.braswell@gmail.com>
2016-08-26 09:56:36 -0400

    move comment back to the right place

--- addon/adapters/contributor.js
+++ addon/adapters/contributor.js
@@ -23,13 +23,13 @@ export default OsfAdapter.extend({
                     return `${base}${id.split('-').pop()}/`;
                 }
 
+                // Needed for Ember Data to update the inverse record's (the node's) relationship
                 var requestUrl = `${base}?embed=node`;
 
                 if (!sendEmail) {
                     requestUrl += `&send_email=false`;
                 }
 
-                // Needed for Ember Data to update the inverse record's (the node's) relationship
                 return requestUrl;
             } else {
                 throw new Error('Trying to add a contributor to a Node that hasn\'t been loaded into the store');
