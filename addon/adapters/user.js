ba88b7713536cba36da70c9309b8aef87bc8cf40
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-09-08 13:43:15 -0400

    Override findHasMany on user adapter to embed root and parent when fetching user nodes.

--- addon/adapters/user.js
+++ addon/adapters/user.js
@@ -1,4 +1,24 @@
 import OsfAdapter from './osf-adapter';
+import Ember from 'ember';
 
 export default OsfAdapter.extend({
+    findHasMany(store, snapshot, url, relationship) {
+        var id = snapshot.id;
+        var type = snapshot.modelName;
+
+        url = this.urlPrefix(url, this.buildURL(type, id, null, 'findHasMany'));
+
+        // If fetching user nodes, will embed root and parent.
+        if (relationship.type === 'node') {
+            var embedParams = 'embed=parent&embed=root';
+            if (snapshot.record.get('query-params')) {
+                var queryParams = Ember.$.param(snapshot.record.get('query-params'));
+                url += '?' + queryParams;
+                url += '&' + embedParams;
+            } else {
+                url += '?' + embedParams;
+            }
+        }
+        return this.ajax(url, 'GET');
+    }
 });
