ca6d2e926e59262fc68da8c655b2a2ebd368c98a
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-10-25 15:54:56 -0400

    Move preprint adapters from preprint repo to ember-osf repo.

---
+++ addon/adapters/preprint.js
@@ -0,0 +1,24 @@
+import OsfAdapter from './osf-adapter';
+
+export default OsfAdapter.extend({
+    // Override _buildRelationshipURL on ember-osf.  Instead of relationship link, need a PATCH to self link
+    _buildRelationshipURL: function _buildRelationshipURL(snapshot) {
+        var url = null;
+        if (snapshot.record.get('links.self')) {
+            url = snapshot.record.get('links.self');
+        }
+        return url;
+    },
+    // Override _doRelatedRequest on ember-osf.  Need to serializer preprint instead of file.
+    _doRelatedRequest: function _doRelatedRequest(store, snapshot, relatedSnapshots, relationship, url) {
+            var isBulk = false;
+            var serializer = store.serializerFor('preprint');
+            var data = serializer.serialize(snapshot);
+
+            return this.ajax(url, 'PATCH', {
+                data: data,
+                isBulk: isBulk
+            });
+        },
+
+});
