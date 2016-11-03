7fd6abdec742ccd418b67903dae0cb23eb041515
Barrett K. Harber <barrett.harber@gmail.com>
2016-11-03 14:16:09 -0400

    Update preprint-provider (moved out of ember-preprints)

--- addon/models/preprint-provider.js
+++ addon/models/preprint-provider.js
@@ -1,12 +1,20 @@
 import DS from 'ember-data';
-import OsfModel from './osf-model';
+import OsfModel from 'ember-osf/models/osf-model';
 
 export default OsfModel.extend({
     name: DS.attr('string'),
-    logo_path: DS.attr('string'),
-    banner_path: DS.attr('string'),
+    logoPath: DS.attr('string'),
+    bannerPath: DS.attr('string'),
     description: DS.attr('string'),
-
+    advisoryBoard: DS.attr('string'),
+    emailContact: DS.attr('string'),
+    emailSupport: DS.attr('string'),
+    socialTwitter: DS.attr('string'),
+    socialFacebook: DS.attr('string'),
+    socialInstagram: DS.attr('string'),
+    headerText: DS.attr('string'),
+    subjectsAcceptable: DS.attr(),
     // Relationships
+    taxonomies: DS.hasMany('taxonomy'),
     preprints: DS.hasMany('preprint', { inverse: 'provider', async: true }),
 });
