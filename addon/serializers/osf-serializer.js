9bfb62aae2fc18728fea5546ab64fa4ed0aea886
Dawn Pattison <pattison.dawn.r@gmail.com>
2016-12-21 13:46:16 -0500

    Update docstring.

--- addon/serializers/osf-serializer.js
+++ addon/serializers/osf-serializer.js
@@ -115,8 +115,8 @@ export default DS.JSONAPISerializer.extend({
     },
 
     normalizeArrayResponse(store, primaryModelClass, payload, id, requestType) { // jshint ignore:line
-        // Ember data does not yet support pagination. For any request that returns more than one result, extract
-        //  links.meta from the payload links section, and add to the model metadata manually.
+        // Ember data does not yet support pagination. For any request that returns more than one result, add pagination data
+        // under meta, and then calculate total pages to be loaded.
         let documentHash = this._super(...arguments);
         documentHash.meta = documentHash.meta || {};
         documentHash.meta.pagination = Ember.$.extend(true, {}, Ember.get(payload || {}, 'meta'));
