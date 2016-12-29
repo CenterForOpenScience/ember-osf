8d77e48042b1ec17940ea601a0c03eb0b11726fd
Henrique Moco <hmoco94@vt.edu>
2016-12-29 15:31:02 -0500

    style fixes

--- tests/dummy/app/components/wiki-detail/component.js
+++ tests/dummy/app/components/wiki-detail/component.js
@@ -2,13 +2,12 @@ import Ember from 'ember';
 import layout from './template';
 import { authenticatedAJAX } from 'ember-osf/utils/ajax-helpers';
 
-
 export default Ember.Component.extend({
     layout,
     showContent: false,
     wikiContent: '',
     actions: {
-        toggleContent: function(){
+        toggleContent: function() {
             Ember.run.once(this, function() {
                 let url = this.get('wiki.links.download');
                 authenticatedAJAX({
