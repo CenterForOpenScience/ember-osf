cf0cf331658aea49283391690e430449ccf3e638
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-01-10 16:21:02 -0500

    Space.

--- addon/utils/load-relationship.js
+++ addon/utils/load-relationship.js
@@ -11,6 +11,7 @@ export default function loadAll(model, relationship, dest, options = {}) {
 
     return model.query(relationship, query).then(results => {
         dest.pushObjects(results.toArray());
+        
         if (results.meta) {
             var total = results.meta.pagination.total;
             var pageSize = results.meta.pagination.per_page;
