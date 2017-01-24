d37c2515eb84eb825bb393f385cda5f090fdb28a
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-01-24 15:40:47 -0500

    Add integration test for search result.

--- tests/integration/components/search-result/component-test.js
+++ tests/integration/components/search-result/component-test.js
@@ -8,17 +8,15 @@ moduleForComponent('search-result', 'Integration | Component | search result', {
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+    this.set('addFilter', () => {});
+    this.set('obj', {
+        title: 'Hello'
+    });
+    this.render(hbs`{{search-result
+        addFilter=(action addFilter)
+        obj=obj
+  
+    }}`);
 
-  this.render(hbs`{{search-result}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#search-result}}
-      template block text
-    {{/search-result}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Hello');
 });
