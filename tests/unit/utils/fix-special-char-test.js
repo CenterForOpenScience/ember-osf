85b56a785a05fdeddb891888f93e7801948bfbb8
Sherif <sherif@cos.io>
2017-01-25 11:00:35 -0500

    Add fix-special-char util to ember-osf Ticket ID:EOSF-202

---
+++ tests/unit/utils/fix-special-char-test.js
@@ -0,0 +1,10 @@
+import fixSpecialChar from 'dummy/utils/fix-special-char';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | fix special char');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = fixSpecialChar();
+  assert.ok(result);
+});
