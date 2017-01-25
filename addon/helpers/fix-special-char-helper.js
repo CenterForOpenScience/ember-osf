85db9e87a773a7499b7b94264cca7d48682217c2
Sherif <sherif@cos.io>
2017-01-25 15:20:04 -0500

    Adjust indentation

--- addon/helpers/fix-special-char-helper.js
+++ addon/helpers/fix-special-char-helper.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import fixSpecialChar from 'dummy/utils/fix-special-char';
 
 export function fixSpecialCharHelper(params/*, hash*/) {
-  return params ? fixSpecialChar(params[0]) : params;
+    return params ? fixSpecialChar(params[0]) : params;
 }
 
 export default Ember.Helper.helper(fixSpecialCharHelper);
