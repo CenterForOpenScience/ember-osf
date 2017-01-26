fb58cb9fa976c67bd5a6e1a28c5e7856c263ccd7
Andy Boughton <abought@gmail.com>
2017-01-26 11:56:07 -0500

    Add docstrings for new helpers
    [ci skip]
    [#EOSF-452]

--- addon/transforms/fixstring.js
+++ addon/transforms/fixstring.js
@@ -8,12 +8,12 @@ import fixSpecialChars from '../utils/fix-special-char';
  */
 
 /**
-  Custom field transform that abstracts away bad API behavior. In certain cases the server will insert HTML escape
-   sequences into text. This allows them to be correctly and transparently used in templates without manually fixing
-   these characters for display on each use.
+
+  Custom string field transform that uses the `fix-special-char` utility function to clean up malformed text sent
+  from the server. This allows string fields to be correctly and transparently used in templates without manually fixing
+  these characters for display on each use.
 
    This transform is used when `fixstring` is passed as the type parameter to the DS.attr function.
-   It replaces `&amp;` with `&`.
     ```app/models/score.js
      import DS from 'ember-data';
      export default DS.Model.extend({
@@ -21,7 +21,8 @@ import fixSpecialChars from '../utils/fix-special-char';
      });
    ```
   @class fixstring
-  @extends DS.Transform
+  @extends DS.StringTransform
+  @uses fix-special-char
  */
 export default DS.StringTransform.extend({
     deserialize(serialized) {
--- addon/utils/fix-special-char.js
+++ addon/utils/fix-special-char.js
@@ -1,3 +1,21 @@
+/**
+ * @module ember-osf
+ * @submodule utils
+ */
+
+/**
+ * @class fix-special-char
+ */
+
+/**
+ * This function is useful for fixing a bad API behavior. In certain cases the server will insert HTML escape
+ * sequences into text, and this will replace `&amp;` sequences with `&`. Template helper and ember-data field versions
+ * of this function are available.
+ *
+ * @method fixSpecialChar
+ * @param {String} inputString A string value to be transformed
+ * @return {String|null}
+ */
 export default function fixSpecialChar(inputString) {
     return inputString ? inputString.replace(/&amp;/g, '&') : inputString;
 }
