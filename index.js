77324d1793751a242545272d070107604a2f336a
Andy Boughton <abought@gmail.com>
2017-01-27 13:11:56 -0500

    Flatten config files in a backwards-compatible way.
     Also move predefined backends into a separate file
    
    [#EOSF-452]

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
 var compileSass = require('broccoli-sass-source-maps');
 
+// Fetch a list of known backends. The user can always choose to override any of these URLs via ENV vars
+var knownBackends = require('./config/backends');
+
 module.exports = {
     name: 'ember-osf',
     blueprintsPath: function() {
@@ -13,12 +16,12 @@ module.exports = {
     },
     config: function(environment, ENV) {
         let BACKEND = process.env.BACKEND || 'local';
-        let SETTINGS = {};
-        try {
-            SETTINGS = config.get(BACKEND);
-        } catch (e) {
-            console.log(`WARNING: you\'ve specified a backend '${BACKEND}' that you have not configured in your config/local.yml`);
-        }
+        // Settings required to configure the developer application, primarily for OAuth2
+        let OAUTH_SETTINGS = {};
+        // Backwards compatibility: old config.yml files were nested, with keys like "local", "test", etc.
+        // New ones are flat- you specify the values you want once. If there is no <backendname> key, assume
+        // this is a flat config file and access keys accordingly.
+        OAUTH_SETTINGS = config['BACKEND'] || config;
 
         // For i18n
         ENV.i18n = {
@@ -26,91 +29,26 @@ module.exports = {
         };
 
         ENV.OSF = {
-            clientId: SETTINGS.CLIENT_ID,
-            scope: SETTINGS.OAUTH_SCOPES,
+            clientId: OAUTH_SETTINGS.CLIENT_ID,
+            scope: OAUTH_SETTINGS.OAUTH_SCOPES,
             apiNamespace: 'v2', // URL suffix (after host)
             backend: BACKEND,
-            redirectUri: SETTINGS.REDIRECT_URI
+            redirectUri: OAUTH_SETTINGS.REDIRECT_URI
         };
 
-        switch (BACKEND) {
-            case 'local':
-                ENV.OSF = {
-                    url: 'http://localhost:5000/',
-                    apiUrl: 'http://localhost:8000',
+        // Fetch configuration information for the application
+        var backendConfig = knownBackends[BACKEND] || {};
 
-                    // Where to direct the user for cookie-based authentication
-                    cookieLoginUrl: 'http://localhost:8080/login',
-                    // Where to direct the user for oauth2-based authentication
-                    oauthUrl: 'http://localhost:8080/oauth2/profile',
-                    renderUrl: 'http://localhost:7778/render',
-                    waterbutlerUrl : 'http://localhost:7777/',
-                    helpUrl: 'http://localhost:4200/help',
-
-                    accessToken: SETTINGS.PERSONAL_ACCESS_TOKEN,
-                    isLocal: true
-                };
-                break;
-            case 'stage':
-                ENV.OSF = {
-                    url: 'https://staging.osf.io/',
-                    apiUrl: 'https://staging-api.osf.io',
-                    cookieLoginUrl: 'https://staging-accounts.osf.io/login',
-                    oauthUrl: 'https://staging-accounts.osf.io/oauth2/authorize',
-                    renderUrl: 'https://staging-mfr.osf.io/render',
-                    waterbutlerUrl: 'http://staging-files.osf.io/',
-                    helpUrl: 'http://help.osf.io'
-                };
-                break;
-            case 'stage2':
-                ENV.OSF = {
-                    url: 'https://staging2.osf.io/',
-                    apiUrl: 'https://staging2-api.osf.io',
-                    cookieLoginUrl: 'https://staging2-accounts.osf.io/login',
-                    oauthUrl: 'https://staging2-accounts.osf.io/oauth2/authorize',
-                    renderUrl: 'https://staging2-mfr.osf.io/render',
-                    waterbutlerUrl: 'http://staging2-files.osf.io/',
-                    helpUrl: 'http://help.osf.io'
-                };
-                break;
-            case 'stage3':
-                ENV.OSF = {
-                    url: 'https://staging3.osf.io/',
-                    apiUrl: 'https://staging3-api.osf.io',
-                    cookieLoginUrl: 'https://staging3-accounts.osf.io/login',
-                    oauthUrl: 'https://staging3-accounts.osf.io/oauth2/authorize',
-                    renderUrl: 'https://staging3-mfr.osf.io/render',
-                    waterbutlerUrl: 'http://staging3-files.osf.io/',
-                    helpUrl: 'http://help.osf.io'
-                };
-                break;
-            case 'test':
-                ENV.OSF = {
-                    url: 'https://test.osf.io/',
-                    apiUrl: 'https://test-api.osf.io',
-                    cookieLoginUrl: 'https://test-accounts.osf.io/login',
-                    oauthUrl: 'https://test-accounts.osf.io/oauth2/authorize',
-                    renderUrl: 'https://test-mfr.osf.io/render',
-                    waterbutlerUrl: 'http://test-files.osf.io/',
-                    helpUrl: 'http://help.osf.io'
-                };
-                break;
-            case 'prod':
-                console.log(`WARNING: you\'ve specified production as a backend. Please do not use production for testing or development purposes`);
-                ENV.OSF = {
-                    url: 'https://osf.io/',
-                    apiUrl: 'https://api.osf.io',
-                    cookieLoginUrl: 'https://accounts.osf.io/login',
-                    oauthUrl: 'https://accounts.osf.io/oauth2/authorize',
-                    renderUrl: 'https://mfr.osf.io/render',
-                    waterbutlerUrl: 'http://files.osf.io/',
-                    helpUrl: 'http://help.osf.io'
-
-                };
-                break;
-            default:
-                break;
+        if (BACKEND === 'local') {
+            backendConfig.accessToken = OAUTH_SETTINGS.PERSONAL_ACCESS_TOKEN;
+            backendConfig.isLocal = true;
+        } else if (BACKEND === 'prod') {
+            console.log("WARNING: you've specified production as a backend. Please do not use production for testing or development purposes");
         }
+
+        // TODO: Deal with incomplete or unrecognized config? (since config comes from env variables, backends, or both, need to check at end)
+        ENV.OSF = backendConfig;
+
         ENV['ember-simple-auth'] = {
             authorizer: 'authorizer:osf-token'
         };
