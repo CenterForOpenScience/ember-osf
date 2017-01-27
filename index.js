8118a055f888f5347015ea899c6ad905f2882415
Andy Boughton <abought@gmail.com>
2017-01-27 11:43:30 -0500

    Reorg settings into separate dictionaries
    [#EOSF-449]

--- index.js
+++ index.js
@@ -33,68 +33,83 @@ module.exports = {
             redirectUri: SETTINGS.REDIRECT_URI
         };
 
-        if (BACKEND === 'local') {
-            ENV.OSF.url = 'http://localhost:5000/';
-            ENV.OSF.apiUrl = 'http://localhost:8000';
+        switch (BACKEND) {
+            case 'local':
+                ENV.OSF = {
+                    url: 'http://localhost:5000/',
+                    apiUrl: 'http://localhost:8000',
 
-            // Where to direct the user for cookie-based authentication
-            ENV.OSF.cookieLoginUrl = 'http://localhost:8080/login';
-            // Where to direct the user for oauth2-based authentication
-            ENV.OSF.oauthUrl = 'http://localhost:8080/oauth2/profile';
-            ENV.OSF.renderUrl = 'http://localhost:7778/render';
-            ENV.OSF.waterbutlerUrl = 'http://localhost:7777/';
-            ENV.OSF.helpUrl = 'http://localhost:4200/help';
+                    // Where to direct the user for cookie-based authentication
+                    cookieLoginUrl: 'http://localhost:8080/login',
+                    // Where to direct the user for oauth2-based authentication
+                    oauthUrl: 'http://localhost:8080/oauth2/profile',
+                    renderUrl: 'http://localhost:7778/render',
+                    waterbutlerUrl : 'http://localhost:7777/',
+                    helpUrl: 'http://localhost:4200/help',
 
-            ENV.OSF.accessToken = SETTINGS.PERSONAL_ACCESS_TOKEN;
-            ENV.OSF.isLocal = true;
-        }
-        if (BACKEND === 'stage') {
-            ENV.OSF.url = 'https://staging.osf.io/';
-            ENV.OSF.apiUrl = 'https://staging-api.osf.io';
-            ENV.OSF.cookieLoginUrl = 'https://staging-accounts.osf.io/login';
-            ENV.OSF.oauthUrl = 'https://staging-accounts.osf.io/oauth2/authorize';
-            ENV.OSF.renderUrl = 'https://staging-mfr.osf.io/render';
-            ENV.OSF.waterbutlerUrl = 'http://staging-files.osf.io/';
-            ENV.OSF.helpUrl = 'http://help.osf.io';
-        }
-        if (BACKEND === 'stage2') {
-            ENV.OSF.url = 'https://staging2.osf.io/';
-            ENV.OSF.apiUrl = 'https://staging2-api.osf.io';
-            ENV.OSF.cookieLoginUrl = 'https://staging2-accounts.osf.io/login';
-            ENV.OSF.oauthUrl = 'https://staging2-accounts.osf.io/oauth2/authorize';
-            ENV.OSF.renderUrl = 'https://staging2-mfr.osf.io/render';
-            ENV.OSF.waterbutlerUrl = 'http://staging2-files.osf.io/';
-            ENV.OSF.helpUrl = 'http://help.osf.io';
-        }
-        // TODO: backend needs to go away, env's need to be from environment vars on build.
-        if (BACKEND === 'stage3') {
-            ENV.OSF.url = 'https://staging3.osf.io/';
-            ENV.OSF.apiUrl = 'https://staging3-api.osf.io';
-            ENV.OSF.cookieLoginUrl = 'https://staging3-accounts.osf.io/login';
-            ENV.OSF.oauthUrl = 'https://staging3-accounts.osf.io/oauth2/authorize';
-            ENV.OSF.renderUrl = 'https://staging3-mfr.osf.io/render';
-            ENV.OSF.waterbutlerUrl = 'http://staging3-files.osf.io/';
-            ENV.OSF.helpUrl = 'http://help.osf.io';
-        }
-        if (BACKEND === 'test') {
-            ENV.OSF.url = 'https://test.osf.io/';
-            ENV.OSF.apiUrl = 'https://test-api.osf.io';
-            ENV.OSF.cookieLoginUrl = 'https://test-accounts.osf.io/login';
-            ENV.OSF.oauthUrl = 'https://test-accounts.osf.io/oauth2/authorize';
-            ENV.OSF.renderUrl = 'https://test-mfr.osf.io/render';
-            ENV.OSF.waterbutlerUrl = 'http://test-files.osf.io/';
-            ENV.OSF.helpUrl = 'http://help.osf.io';
-        }
-        if (BACKEND === 'prod') {
-            console.log(`WARNING: you\'ve specified production as a backend. Please do not use production for testing or development purposes`);
-            ENV.OSF.url = 'https://osf.io/';
-            ENV.OSF.apiUrl = 'https://api.osf.io';
-            ENV.OSF.cookieLoginUrl = 'https://accounts.osf.io/login';
-            ENV.OSF.oauthUrl = 'https://accounts.osf.io/oauth2/authorize';
-            ENV.OSF.renderUrl = 'https://mfr.osf.io/render';
-            ENV.OSF.waterbutlerUrl = 'http://files.osf.io/';
-            ENV.OSF.helpUrl = 'http://help.osf.io';
+                    accessToken: SETTINGS.PERSONAL_ACCESS_TOKEN,
+                    isLocal: true
+                };
+                break;
+            case 'stage':
+                ENV.OSF = {
+                    url: 'https://staging.osf.io/',
+                    apiUrl: 'https://staging-api.osf.io',
+                    cookieLoginUrl: 'https://staging-accounts.osf.io/login',
+                    oauthUrl: 'https://staging-accounts.osf.io/oauth2/authorize',
+                    renderUrl: 'https://staging-mfr.osf.io/render',
+                    waterbutlerUrl: 'http://staging-files.osf.io/',
+                    helpUrl: 'http://help.osf.io'
+                };
+                break;
+            case 'stage2':
+                ENV.OSF = {
+                    url: 'https://staging2.osf.io/',
+                    apiUrl: 'https://staging2-api.osf.io',
+                    cookieLoginUrl: 'https://staging2-accounts.osf.io/login',
+                    oauthUrl: 'https://staging2-accounts.osf.io/oauth2/authorize',
+                    renderUrl: 'https://staging2-mfr.osf.io/render',
+                    waterbutlerUrl: 'http://staging2-files.osf.io/',
+                    helpUrl: 'http://help.osf.io'
+                };
+                break;
+            case 'stage3':
+                ENV.OSF = {
+                    url: 'https://staging3.osf.io/',
+                    apiUrl: 'https://staging3-api.osf.io',
+                    cookieLoginUrl: 'https://staging3-accounts.osf.io/login',
+                    oauthUrl: 'https://staging3-accounts.osf.io/oauth2/authorize',
+                    renderUrl: 'https://staging3-mfr.osf.io/render',
+                    waterbutlerUrl: 'http://staging3-files.osf.io/',
+                    helpUrl: 'http://help.osf.io'
+                };
+                break;
+            case 'test':
+                ENV.OSF = {
+                    url: 'https://test.osf.io/',
+                    apiUrl: 'https://test-api.osf.io',
+                    cookieLoginUrl: 'https://test-accounts.osf.io/login',
+                    oauthUrl: 'https://test-accounts.osf.io/oauth2/authorize',
+                    renderUrl: 'https://test-mfr.osf.io/render',
+                    waterbutlerUrl: 'http://test-files.osf.io/',
+                    helpUrl: 'http://help.osf.io'
+                };
+                break;
+            case 'prod':
+                console.log(`WARNING: you\'ve specified production as a backend. Please do not use production for testing or development purposes`);
+                ENV.OSF = {
+                    url: 'https://osf.io/',
+                    apiUrl: 'https://api.osf.io',
+                    cookieLoginUrl: 'https://accounts.osf.io/login',
+                    oauthUrl: 'https://accounts.osf.io/oauth2/authorize',
+                    renderUrl: 'https://mfr.osf.io/render',
+                    waterbutlerUrl: 'http://files.osf.io/',
+                    helpUrl: 'http://help.osf.io'
 
+                };
+                break;
+            default:
+                break;
         }
         ENV['ember-simple-auth'] = {
             authorizer: 'authorizer:osf-token'
