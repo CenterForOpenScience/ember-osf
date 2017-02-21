f922385ece51fe13c7dc1d0ac385a1a9e18a630f
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-02-21 13:52:10 -0500

    Assume properties are a dictionary.

--- addon/metrics-adapters/keen.js
+++ addon/metrics-adapters/keen.js
@@ -39,7 +39,7 @@ export default BaseAdapter.extend({
         window.contextVars = {};
         window.contextVars.currentUser = this.userContextVars();
         window.contextVars.node = this.nodeContextVars(node);
-        return this.KeenTracker().getInstance().trackPrivateEvent(collection, { interaction: properties }, node);
+        return this.KeenTracker().getInstance().trackPrivateEvent(collection, properties, node);
     },
 
     willDestroy() {},
