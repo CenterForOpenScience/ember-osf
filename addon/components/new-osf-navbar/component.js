06ee8ff90538441f2cb1215a2bc9742d03d339ae
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-02-24 14:15:15 -0500

    Loop through OSF services to display in navbar.

--- addon/components/new-osf-navbar/component.js
+++ addon/components/new-osf-navbar/component.js
@@ -1,6 +1,35 @@
 import Ember from 'ember';
 import layout from './template';
+import config from 'ember-get-config';
 
 export default Ember.Component.extend({
-  layout
+    layout,
+    host: config.OSF.url,
+    currentService: "HOME",
+    osfServices: Ember.computed('host', function() {
+        return [
+            {
+                name: "HOME",
+                url: `${this.get('host')}`
+            },
+            {
+                name: "PREPRINTS",
+                url: `${this.get('host')}preprints/`
+            },
+            {
+                name: "REGISTRIES",
+                url: `${this.get('host')}registries/`
+            },
+            {
+                name: "MEETINGS",
+                url: `${this.get('host')}meetings/`
+            }
+        ];
+    }),
+    actions: {
+        switchService(serviceName) {
+            this.set('currentService', serviceName);
+        }
+    }
+
 });
