1ec1867a7330c132d8541adced362624ed5b95f3
Barrett K. Harber <barrett.harber@gmail.com>
2017-03-15 12:42:37 -0400

    baseURL -> rootURL

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -39,7 +39,7 @@ module.exports = function(environment) {
         ],
         modulePrefix: 'dummy',
         environment: environment,
-        baseURL: '/',
+        rootURL: '/',
         locationType: 'auto',
 
         EmberENV: {
@@ -74,7 +74,7 @@ module.exports = function(environment) {
 
     if (environment === 'test') {
         // Testem prefers this...
-        ENV.baseURL = '/';
+        ENV.rootURL = '/';
         ENV.locationType = 'none';
 
         // keep test console output quieter
