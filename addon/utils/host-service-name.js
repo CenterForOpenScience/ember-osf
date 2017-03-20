2c9621c74c695ea63ed889b06a9adc1abaae1573
Sherif Abdelhamid <sherif@cos.io>
2017-03-20 14:12:44 -0400

    Add new utility

---
+++ addon/utils/host-service-name.js
@@ -0,0 +1,23 @@
+import config from 'ember-get-config';
+
+/**
+ * @module ember-osf
+ * @submodule utils
+ */
+
+/**
+ * @class host-service-name-helper
+ */
+
+/**
+ * This function is useful for retrieving the hostname from the environment.js for the hosting app. This is needed to
+ * allow ember-osf addons identify their hosting apps.
+ *
+ * @method hostservicename
+ * @return {String}
+ */
+export default function hostServiceName() {
+    return config.modulePrefix;
+}
+
+export { hostServiceName };
---
+++ app/utils/host-service-name.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-osf/utils/host-service-name';
