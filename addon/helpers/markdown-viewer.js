b43e76852eb0ed2f2474c885e31218cb85de1c12
Sherif Abdelhamid <sherif@cos.io>
2017-04-21 13:48:10 -0400

    Apply helper changes

--- addon/helpers/markdown-viewer.js
+++ addon/helpers/markdown-viewer.js
@@ -17,25 +17,7 @@ Render markdown text for selected rules.
 export function markdownViewer(params) {
 
     const [markdown] = params;
-    const html = markdownit('zero', {
-        html: false,        // Enable HTML tags in source
-
-        linkify: false,     // Autoconvert URL-like text to links
-
-        typographer: false, // Enable some language-neutral replacement + quotes beautification
-
-        quotes: '“”‘’',
-        // Double + single quotes replacement pairs, when typographer enabled,
-        // and smartquotes on. Could be either a String or an Array.
-        //
-        // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-        // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
-
-        xhtmlOut: false,    // Use '/' to close single tags (<br />).
-        // This is only for full CommonMark compatibility.
-
-        breaks: true,      // Convert '\n' in paragraphs into <br>
-    }).enable(['list', 'newline', 'emphasis', 'paragraph']);
+    const html = markdownit('zero').enable(['list', 'newline', 'emphasis', 'paragraph', 'link']);
     const renderedHtml = html.render(markdown);
 
     return htmlSafe(renderedHtml);
