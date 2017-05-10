edcc4f189c886773ac72ad122504c2aee1f00654
Dawn Pattison <pattison.dawn.r@gmail.com>
2017-05-09 22:52:56 -0400

    Add stub for i18n service.

--- tests/integration/components/total-share-results/component-test.js
+++ tests/integration/components/total-share-results/component-test.js
@@ -1,8 +1,24 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import tHelper from "ember-i18n/helper";
+import Ember from 'ember';
 
+// Stub i18n service
+const i18nStub = Ember.Service.extend({
+    t: function(word) {
+        const translated = {
+            'searchablePreprints': `searchable preprints`,
+        };
+        return translated[word];
+    }
+});
 moduleForComponent('total-share-results', 'Integration | Component | total share results', {
-  integration: true
+  integration: true,
+  beforeEach() {
+      this.registry.register('helper:t', tHelper);
+      this.register('service:i18n', i18nStub);
+      this.inject.service('i18n', { as: 'i18n' });
+  }
 });
 
 test('it renders', function(assert) {
@@ -10,11 +26,11 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
     this.set('shareTotal', 100);
-    this.set('shareTotalText', 'searchable preprints');
+    this.set('shareTotalText', 'searchablePreprints');
 
     this.render(hbs`{{total-share-results
         shareTotal=shareTotal
         shareTotalText=shareTotalText
     }}`);
-    assert.ok(this.$().text().trim().includes('100 searchable preprints'));
+    assert.strictEqual(this.$('span')[0].innerText, 'searchable preprints');
 });
