72e438d46db28e49e4dc2104e5ace488488dc19f
Henrique Harman <henriqueharman@gmail.com>
2017-06-19 14:32:17 -0400

    Use t-plus as a helper instead of a component

---
+++ tests/unit/helpers/t-plus-test.js
@@ -0,0 +1,12 @@
+
+import { tPlus } from 'dummy/helpers/t-plus';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | t plus');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = tPlus([42]);
+  assert.ok(result);
+});
+
