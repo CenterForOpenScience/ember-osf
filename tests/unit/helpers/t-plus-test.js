9738ed9d2f9e2e70e6ac1f2475f2a390fbfa043b
Henrique Harman <henriqueharman@gmail.com>
2017-06-19 15:25:05 -0400

    Add tests for t-plus

--- tests/unit/helpers/t-plus-test.js
+++ tests/unit/helpers/t-plus-test.js
@@ -1,12 +1,37 @@
-
 import { tPlus } from 'dummy/helpers/t-plus';
 import { module, test } from 'qunit';
 
 module('Unit | Helper | t plus');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = tPlus([42]);
-  assert.ok(result);
+
+test('it works with defaults 11', function(assert) {
+    i18n = this.container.lookup('service:i18n');
+    i18n.addTranslations('en', {'general.help': 'What is a {{preprintWords.preprint}}?'});
+
+    let result = tPlus('general.help');
+    assert.equal(result, 'What is a preprint?');
 });
 
+test('provider wants paper 11', function(assert) {
+    theme = this.container.lookup('service:theme');
+    theme.set('provider', {
+        preprintWord: 'paper'
+    });
+    i18n = this.container.lookup('service:i18n');
+    i18n.addTranslations('en', {'general.help': 'What is a {{preprintWords.preprint}}?'});
+
+    let result = tPlus('general.help');
+    assert.equal(result, 'What is a paper?');
+});
+
+test('provider wants preprints 11', function(assert) {
+    theme = this.container.lookup('service:theme');
+    theme.set('provider', {
+        preprintWord: 'preprint'
+    });
+    i18n = this.container.lookup('service:i18n');
+    i18n.addTranslations('en', {'general.help': 'What are {{preprintWords.Preprints}}?'});
+
+    let result = tPlus('general.help');
+    assert.equal(result, 'What are Preprints?');
+});
