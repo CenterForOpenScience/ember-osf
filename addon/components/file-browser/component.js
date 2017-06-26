5000268bc14e87a993fa3adcd6b970d047fcddf9
Henrique Harman <henriqueharman@gmail.com>
2017-06-26 13:47:38 -0400

    Shift logic works

--- addon/components/file-browser/component.js
+++ addon/components/file-browser/component.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 import layout from './template';
 
 import loadAll from 'ember-osf/utils/load-relationship';
+import { authenticatedAJAX } from 'ember-osf/utils/ajax-helpers';
 
 /**
  * @module ember-osf
@@ -104,23 +105,12 @@ export default Ember.Component.extend({
     items: Ember.computed.readOnly('currentParent.childItems.firstObject.childItems'),
     itemsLoaded: Ember.computed.readOnly('currentParent.childItemsLoaded'),
     selectedItems: Ember.computed.filterBy('items', 'isSelected', true),
-
     loadedChanged: Ember.observer('itemsLoaded', function() {
         let containerWidth = this.$().width();
         this.set('itemWidth', containerWidth);
     }),
 
     actions: {
-        // selectItem(item) {
-        //     item.toggleProperty('isSelected');
-        //     // item.set('isSelected', true);
-        //     if (item.get('isFile') && this.get('selectFile')) {
-        //         this.sendAction('selectFile', unwrapItem(item));
-        //     }
-        //     if (item.get('isNode') && this.get('selectNode')) {
-        //         this.sendAction('selectNode', unwrapItem(item));
-        //     }
-        // },
         selectItem(item) {
             if (this.get('selectedItems.length')) {
                 for (var item_ of this.get('selectedItems')) {
@@ -128,30 +118,58 @@ export default Ember.Component.extend({
                 }
             }
             item.set('isSelected', true);
+            this.set('shiftAnchor', item);
         },
         selectMultiple(item, toggle) {
             if (toggle) {
                 item.toggleProperty('isSelected');
             } else {
+                let items = this.get('items');
+                let anchor = this.get('shiftAnchor');
+                if (anchor) {
+                    let max = Math.max(items.indexOf(anchor), items.indexOf(item));
+                    let min = Math.min(items.indexOf(anchor), items.indexOf(item));
+                    for (var item_ of this.get('items')) {
+                        item_.set('isSelected', item_ === item || item_ === anchor || (items.indexOf(item_) > min && items.indexOf(item_) < max));
+                    }
+                }
                 item.set('isSelected', true);
             }
+            Ember.run.next(this, function(){
+                if (this.get('selectedItems.length') === 1) {
+                    this.set('shiftAnchor', item)
+                }
+            });
         },
-
+        viewItem() {
+            let item = this.get('selectedItems.firstObject');
+            this.sendAction('openItem', unwrapItem(item));
+        },
         openItem(item) {
-            // if (item.get('isFile') && this.get('openFile')) {
             this.sendAction('openFile', unwrapItem(item));
-            // }
-            // if (item.get('isNode') && this.get('openNode')) {
-            //     this.sendAction('openNode', unwrapItem(item));
-            // }
-            // if (item.get('canHaveChildren')) {
-            //     this.send('navigateToItem', item);
-            //     for (item of this.get('items')) {
-            //         item.set('isSelected', false);
-            //     }
-            // }
         },
+        downloadItem() {
+            let downloadLink = this.get('selectedItems.firstObject.links.download');
+            window.location = downloadLink;
+        },
+        deleteItem(){
+            let url = this.get('selectedItems.firstObject.links.download')
+            authenticatedAJAX({
+                url: url,
+                type: 'DELETE',
+                xhrFields: {withCredentials: true}
+            })
+            .done(function(data) {
+                console.log(data);
+                debugger;
+            })
+            .fail(function(data){
+                console.log(data);
+            });
+        },
+        deleteItems() {
 
+        },
         navigateToItem(item) {
             let breadcrumbs = this.get('breadcrumbs');
             let index = breadcrumbs.indexOf(item);
