c56526c07527eda3a3e75e58ad5fece72c0bee49
Henrique Harman <henriqueharman@gmail.com>
2017-07-21 13:23:31 -0400

    Fix tests to use pipe as well

--- tests/unit/helpers/custom-taxonomy-filter-test.js
+++ tests/unit/helpers/custom-taxonomy-filter-test.js
@@ -4,12 +4,12 @@ import { module, test } from 'qunit';
 module('Unit | Helper | custom taxonomy filter');
 
 test('replace very deep custom taxonomy hierarchy with just final subject name', function(assert) {
-    let result = customTaxonomyFilter(['bepress/Fruits/Bananas/Green Bananas/Small Green Bananas/Small Green Bananas with Polka Dots']);
+    let result = customTaxonomyFilter(['bepress|Fruits|Bananas|Green Bananas|Small / Green Bananas|Small Green Bananas with Polka Dots']);
     assert.equal(result, 'Small Green Bananas with Polka Dots');
 });
 
 test('replace shallow custom taxonomy with just final subject name', function(assert) {
-    let result = customTaxonomyFilter(['bepress/Fruits']);
+    let result = customTaxonomyFilter(['bepress|Fruits']);
     assert.equal(result, 'Fruits');
 });
 
@@ -18,7 +18,7 @@ test('return original subject if not a custom taxonomy', function(assert) {
     assert.equal(result, 'Fruits and Bananas');
 });
 
-test('return original subject if not a recognized custom taxonomy', function(assert) {
+test('return original subject if separated with slashes', function(assert) {
     let result = customTaxonomyFilter(['osf/Fruits/Bananas']);
     assert.equal(result, 'osf/Fruits/Bananas');
 });
