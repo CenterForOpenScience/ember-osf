21f8a59b1135dd3a4416cc2e463c254a6f9e0383
Baylee Swenson <baylee@cos.io>
2017-07-25 12:02:44 -0400

    donate banner WIP

---
+++ addon/components/donate-banner/component.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+import layout from './template';
+
+/**
+ * @module ember-osf
+ * @submodule components
+ */
+
+/**
+ * Display the donate banner.  Has an automated script to determine what the banner should look like
+ * based on the the current date.
+ *
+ * Sample usage:
+ * ```handlebars
+ * {{osf-navbar
+ *   loginAction=loginAction
+ * }}
+ * ```
+ *
+ * @class donate-banner
+ */
+export default Ember.Component.extend({
+
+    
+});
