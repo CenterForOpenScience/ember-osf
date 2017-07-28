66ce9eff45536b1b7c286cddf5f0048a51efe1e2
Baylee Swenson <baylee@cos.io>
2017-07-28 10:08:36 -0400

    Added tests and updated images

---
+++ tests/integration/components/donate-banner/component-test.js
@@ -0,0 +1,27 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('donate-banner', 'Integration | Component | donate banner', {
+  integration: true
+});
+
+
+test('it renders', function(assert) {
+
+    this.set('testDate', new Date('July 27, 2017'));
+
+    this.render(hbs`{{donate-banner currentDate=testDate}}`);
+
+    assert.equal(this.$('div').attr('style'), 'display:""');
+
+});
+
+test('does not show', function(assert) {
+
+    this.set('testDate', new Date('June 1, 2017'));
+
+    this.render(hbs`{{donate-banner currentDate=testDate}}`);
+
+    assert.equal(this.$('div').attr('style'), 'display:none');
+
+});
