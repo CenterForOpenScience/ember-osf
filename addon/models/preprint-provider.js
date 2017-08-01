768f619c46fb1c4b078b039a4a1214b2b55d8e84
James C. Davis <james@cos.io>
2017-08-01 15:48:34 -0400

    Added highlighted taxonomies relationship to preprint-provider model

--- addon/models/preprint-provider.js
+++ addon/models/preprint-provider.js
@@ -16,6 +16,7 @@ export default OsfModel.extend({
     shareSource: DS.attr('string'),
     // Relationships
     taxonomies: DS.hasMany('taxonomy'),
+    highlightedTaxonomies: DS.hasMany('taxonomy'),
     preprints: DS.hasMany('preprint', { inverse: 'provider', async: true }),
     licensesAcceptable: DS.hasMany('license', { inverse: null }),
 });
