0605c350e27a693d440d967d57e47ac8cabca8d6
Henrique Harman <henriqueharman@gmail.com>
2017-08-01 13:29:32 -0400

    Use t again and extend it

--- addon/instance-initializers/ember-osf.js
+++ addon/instance-initializers/ember-osf.js
@@ -3,6 +3,23 @@ Automatically expose translations for addon in a way that can be merged in with 
     https://github.com/jamesarosen/ember-i18n/issues/255
  */
 import en from 'ember-osf/locales/en/translations';
+import tHelper from "ember-i18n/helper";
+
+tHelper.reopen({
+    theme: Ember.inject.service(),
+    compute(_, params) {
+        let translations = this.get('i18n._locale.translations');
+        let preprintWord = this.get('theme.provider.preprintWord') || 'preprint';
+        params = params || {};
+        params.preprintWords = {
+            preprint: translations[`preprintWords.${preprintWord}.preprint`],
+            preprints: translations[`preprintWords.${preprintWord}.preprints`],
+            Preprint: translations[`preprintWords.${preprintWord}.Preprint`],
+            Preprints: translations[`preprintWords.${preprintWord}.Preprints`]
+        };
+        return this._super(_, params);
+    }
+})
 
 export function initialize(appInstance) {
     const i18n = appInstance.lookup('service:i18n');
