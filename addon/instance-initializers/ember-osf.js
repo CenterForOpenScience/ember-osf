086487c2812f9ea22fbac0fc7c3dac3d4356c6fc
Henrique Harman <henriqueharman@gmail.com>
2017-08-11 11:33:30 -0400

    Account for provider not being available right away

--- addon/instance-initializers/ember-osf.js
+++ addon/instance-initializers/ember-osf.js
@@ -4,24 +4,38 @@ Automatically expose translations for addon in a way that can be merged in with 
  */
 import Ember from 'ember';
 import en from 'ember-osf/locales/en/translations';
+import tHelper from 'ember-i18n/helper';
 
 export function initialize(appInstance) {
-    const i18n = appInstance.lookup('service:i18n');
-    i18n.reopen({
+    tHelper.reopen({
         theme: Ember.inject.service(),
-        t(_, data) {
-            let translations = this.get('_locale.translations');
-            let preprintWord = this.get('theme.provider.preprintWord') || 'preprint';
+        compute(_, data, provider) {
+            // let _super = this._super.bind(this);
             data = data || {};
-            data.preprintWords = {
+            let translations = this.get('i18n._locale.translations');
+            let preprintWord = this.get('theme.provider.preprintWord');
+            let getTerms = () => ({
                 preprint: translations[`preprintWords.${preprintWord}.preprint`],
                 preprints: translations[`preprintWords.${preprintWord}.preprints`],
                 Preprint: translations[`preprintWords.${preprintWord}.Preprint`],
                 Preprints: translations[`preprintWords.${preprintWord}.Preprints`]
-            };
+            });
+            if (!preprintWord) {
+                if (!provider) {
+                    this.get('theme.provider').then(provider => {
+                        preprintWord = provider.get('preprintWord') || 'preprint';
+                        data.preprintWords = getTerms();
+                        this.recompute(_, data, provider);
+                    });
+                    return null;
+                }
+                preprintWord = provider.get('preprintWord') || 'preprint';
+            }
+            data.preprintWords = getTerms();
             return this._super(_, data);
         }
     });
+    const i18n = appInstance.lookup('service:i18n');
     i18n.addTranslations('en', en);
 }
 
