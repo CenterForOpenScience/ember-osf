7d5dc95663e13893d7a5f530583c66aaa4ad77c5
Chris Seto <chriskseto@gmail.com>
2017-08-18 14:10:37 -0400

    Fix some spacing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,37 +4,37 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
     var app = new EmberAddon(defaults, {
-      sassOptions: {
-          includePaths: [
-              'bower_components/osf-style/sass',
-              'tests/dummy/app/components',
-              'bower_components/c3',
-              'bower_components/bootstrap-daterangepicker',
-          ]
-      },
+        sassOptions: {
+            includePaths: [
+                'bower_components/osf-style/sass',
+                'tests/dummy/app/components',
+                'bower_components/c3',
+                'bower_components/bootstrap-daterangepicker',
+            ]
+        },
 
-      // FIXME
-      // FIXME Will this affect downstream consumers (when they build their apps)?
-      // FIXME
-      // This is really for building the application. It's a hack so we can use the dummy app inside the OSF to test development.
-      // It's for production builds, and we may want to comment it out when needed. Usage:
-      //   ember build --output-path ../osf.io/website/static/ember/ --environment=production --watch
-      //////// Uncomment the lines below to test!
-      // fingerprint: {
-      //     prepend: 'static/ember/'
-      // },
+        // FIXME
+        // FIXME Will this affect downstream consumers (when they build their apps)?
+        // FIXME
+        // This is really for building the application. It's a hack so we can use the dummy app inside the OSF to test development.
+        // It's for production builds, and we may want to comment it out when needed. Usage:
+        //   ember build --output-path ../osf.io/website/static/ember/ --environment=production --watch
+        //////// Uncomment the lines below to test!
+        // fingerprint: {
+        //     prepend: 'static/ember/'
+        // },
 
-      'ember-cli-babel': {
-          includePolyfill: true
-      },
+        'ember-cli-babel': {
+            includePolyfill: true
+        },
 
-      // Exclude any additional assets
-      // The host application should be responsible for importing them
-      'ember-bootstrap': {
-        'bootstrapVersion': 3,
-        'importBootstrapFont': false,
-        'importBootstrapCSS': false
-      }
+        // Exclude any additional assets
+        // The host application should be responsible for importing them
+        'ember-bootstrap': {
+            'bootstrapVersion': 3,
+            'importBootstrapFont': false,
+            'importBootstrapCSS': false
+        }
     });
 
     app.import(path.join(app.bowerDirectory, 'dropzone/dist/basic.css'));
