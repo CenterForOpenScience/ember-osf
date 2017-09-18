e032fa4570babf48d432a4ae67ce7f22459099d3
Abram Booth <abram@cos.io>
2017-09-18 17:32:06 -0400

    Add `user` property to current-user instead

--- addon/services/current-user.js
+++ addon/services/current-user.js
@@ -7,39 +7,13 @@ import Ember from 'ember';
 
 /**
  * Access information about the currently logged in user
- * Proxies all undefined properties to the the user returned by /users/me/
+ *
  * @class current-user
  * @extends Ember.Service
  */
-export default Ember.Service.extend(Ember.ObjectProxy.PrototypeMixin, Ember.PromiseProxyMixin, {
+export default Ember.Service.extend({
     store: Ember.inject.service(),
     session: Ember.inject.service(),
-
-    init() {
-        // Trigger initial KVO cycle
-        // Because Services don't automatically... because why would they :|
-        this.set('promise', this.get('promise'));
-
-        this._super(...arguments);
-    },
-
-    // The promise for PromiseProxyMixin
-    promise: Ember.computed('session.data.authenticated', {
-        get() {
-            let userId = this.get('currentUserId');
-            if (!userId) return Ember.RSVP.resolve(null);
-
-            let user = this.get('store').peekRecord('user', userId);
-            if (user) return Ember.RSVP.resolve(user);
-
-            return this.get('store').findRecord('user', userId);
-        },
-        set(key, value) {
-            // HACK: We need to use the original setter, which is in a closure (go figure), to make the PromiseProxyMixin work
-            return Ember.PromiseProxyMixin.mixins[0].properties.promise._setter.apply(this, [key, value]);
-        }
-    }),
-
 
     /**
      * If logged in, return the ID of the current user, else return null.
@@ -62,6 +36,31 @@ export default Ember.Service.extend(Ember.ObjectProxy.PrototypeMixin, Ember.Prom
      * @return {Promise}
      */
     load() {
-        return this.get('promise');
-    }
+        return new Ember.RSVP.Promise((resolve, reject) => {
+            var currentUserId = this.get('currentUserId');
+            if (currentUserId) {
+                var currentUser = this.get('store').peekRecord('user', currentUserId);
+                if (currentUser) {
+                    resolve(currentUser);
+                } else {
+                    this.get('store').findRecord('user', currentUserId).then((user) => resolve(user), reject);
+                }
+            } else {
+                reject();
+            }
+        });
+    },
+
+    /**
+     * Return an observable promise proxy for the currently logged in user. If no user is logged in, resolves to null.
+     *
+     * @property user
+     * @return Promise proxy object that resolves to a user or null
+     */
+    user: Ember.computed('currentUserId', function() {
+        const ObjectPromiseProxy = Ember.ObjectProxy.extend(Ember.PromiseProxyMixin);
+        return ObjectPromiseProxy.create({
+            promise: this.load().catch(() => null),
+        });
+    }),
 });
