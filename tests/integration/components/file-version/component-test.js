4aca6f027d8b174136c5626138872c59ff493c81
Baylee Swenson <baylee@cos.io>
2017-09-26 12:14:44 -0400

    Fix failing tests

--- tests/integration/components/file-version/component-test.js
+++ tests/integration/components/file-version/component-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 import FactoryGuy, {manualSetup }  from 'ember-data-factory-guy';
 
@@ -16,12 +17,22 @@ test('it renders', function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
     var fileVersion = FactoryGuy.make('file-version');
+    const currentDate = moment().format('YYYY-MM-DD h:mm A');
+
     this.set('fileVersion', fileVersion);
+    this.set('fileVersion', 'modified_utc', currentDate);
+
     this.render(hbs`{{file-version version=fileVersion}}`);
 
     assert.equal(
-        this.$('.file-version').children().eq(1).text(),
-        `${fileVersion.get('size')}`,
-        'Second list element should be a label with file size'
+        this.$('.file-version').children().eq(1).text(), 
+        currentDate, 
+        'Second list element should be a label with the files date'
+    );
+
+    assert.equal(
+        this.$('.file-version').children().eq(2).text(), 
+        0, 
+        'Third list element should be a label with the download count - which is 0'
     );
 });
