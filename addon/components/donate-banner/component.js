85973a4447645b6fc6e2eca903d0d4518b44b329
Baylee Swenson <baylee@cos.io>
2017-11-21 11:39:43 -0500

    Added Giving Tuesday donate banner

--- addon/components/donate-banner/component.js
+++ addon/components/donate-banner/component.js
@@ -33,10 +33,10 @@ export default Ember.Component.extend(AnalyticsMixin, {
             if (currentDate >= changeDates[x].date
                     && (currentDate < endDate)) {
 
-                this.set('bannerBackground', 'banner-' + x);
-                this.set('beforeText', changeDates[x].beforeText);
-                this.set('afterText', changeDates[x].afterText);
-                this.set('linkText', changeDates[x].linkText);
+                this.set('altLg', changeDates[x].altLg);
+                this.set('altSm', changeDates[x].altSm);
+                this.set('largeClass', changeDates[x].largeClass);
+                this.set('smallClass', changeDates[x].smallClass);
                 this.set('style', 'display:""');
                 break;
 
@@ -50,64 +50,30 @@ export default Ember.Component.extend(AnalyticsMixin, {
 
     attributeBindings: ['style'],
     display: '',
-    bannerBackground: '',
-    beforeText: '',
-    afterText: '',
-    linkText: '',
+    alt: '',
+    largeClass: '',
+    smallClass: '',
     currentDate: null,
 
     changeDates: Ember.A([
         {
-            'date': new Date(2017, 7, 7),
-            'beforeText': 'The Center for Open Science (COS) created the OSF and ' +
-                        'a suite of of free products to advance the work of ' +
-                        'the research community. If you value these tools, ' +
-                        'please make a gift to support COS’s efforts to improve ' +
-                        'and scale these services. ',
-            'afterText': '',
-            'linkText': 'Donate Now!'
+            'date': new Date(2017, 10, 20),
+            'largeClass': 'wk-1-lg',
+            'smallClass': 'wk-1-sm',
+            'altLg': 'Things to look forward to: Thanksgiving Thursday, Black Friday, Cyber Monday, AND Giving Tuesday on ' +
+            'Tuesday, November 28th. Please make a gift to support the OSF tools you use and love.',
+            'altSm': 'Giving Tuesday is coming on Tuesday, November 28th. Please make a gift to support the OSF tools you ' +
+            'use and love.',
         },
         {
-            'date': new Date(2017, 7, 14),
-            'beforeText': 'Thousands of researchers use the OSF and its related ' +
-                          'services daily. If you value the OSF, ',
-            'afterText':  ' to support the Center for Open Science ' +
-                          'and its ongoing efforts to improve and advance ' +
-                          'these public goods. ',
-            'linkText':   'make a donation'
+            'date': new Date(2017, 10, 28),
+            'largeClass': 'wk-2-lg',
+            'smallClass': 'wk-2-lg',
+            'altLg': 'Happy Giving Tuesday! Please make a gift to support the OSF tools you use and love.',
+            'altSm': 'Happy Giving Tuesday! Please make a gift to support the OSF tools you use and love.',
         },
         {
-            'date': new Date(2017, 7, 21),
-            'beforeText': 'The Center for Open Science (COS) created the ' +
-                          'OSF and its related services as public goods. ' +
-                          'While these services will always be free to use ' +
-                          'they are not free to build, improve and maintain. ',
-            'afterText':  '',
-            'linkText':   'Please support the OSF and COS with a donation today.'
-        },
-        {
-            'date': new Date(2017, 7, 28),
-            'beforeText': 'The Center for Open Science launched the ' +
-                          'OSF with the goal of creating a service where ' +
-                          'the entire research cycle is supported and ' +
-                          'barriers to accessing data are removed. ',
-            'afterText':  ' to advance the work of researchers with ' +
-                          'a gift today!',
-            'linkText':   "Support COS's efforts"
-        },
-        {
-            'date': new Date(2017, 8, 4),
-            'beforeText': 'At the Center for Open Science (COS), we ' +
-                          'envision a future in which ideas, processes ' +
-                          'and outcomes of research are free and open to ' +
-                          'all. COS relies on contributions to build the ' +
-                          'free products you use and love. Help make the ' +
-                          'vision a reality with a ',
-            'afterText': '',
-            'linkText': 'gift today.'
-        },
-        {
-            'date': new Date(2017, 8, 11)
+            'date': new Date(2018, 10, 29)
         }
     ]),
 });
