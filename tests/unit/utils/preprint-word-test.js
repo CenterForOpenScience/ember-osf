6c1fed94e373da866c7855e01ebd4e5083a7f6ac
Sherif Abdelhamid <sherif@cos.io>
2017-12-07 20:29:13 -0500

    Add preprint-word util

---
+++ tests/unit/utils/preprint-word-test.js
@@ -0,0 +1,44 @@
+import preprintWord from 'dummy/utils/preprint-word';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | preprint word');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = preprintWord();
+  const expected =
+      {
+          document: {
+              plural: 'documents',
+              pluralCapitalized: 'Documents',
+              singular: 'document',
+              singularCapitalized: 'Document',
+          },
+          paper: {
+              plural: 'papers',
+              pluralCapitalized: 'Papers',
+              singular: 'paper',
+              singularCapitalized: 'Paper',
+          },
+          preprint: {
+              plural: 'preprints',
+              pluralCapitalized: 'Preprints',
+              singular: 'preprint',
+              singularCapitalized: 'Preprint',
+          },
+          none: {
+              plural: '',
+              pluralCapitalized: '',
+              singular: '',
+              singularCapitalized: '',
+          },
+          thesis: {
+              plural: 'theses',
+              pluralCapitalized: 'Theses',
+              singular: 'thesis',
+              singularCapitalized: 'Thesis',
+          },
+
+      };
+  assert.deepEqual(result, expected);
+});
