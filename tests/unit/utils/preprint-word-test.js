f38159bac2fa2ef3d45f780d6273438edd3e6cab
Sherif Abdelhamid <sherif@cos.io>
2017-12-08 11:07:27 -0500

    update wording

--- tests/unit/utils/preprint-word-test.js
+++ tests/unit/utils/preprint-word-test.js
@@ -3,8 +3,7 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | preprint word');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('preprint-word works', function(assert) {
   let result = preprintWord();
   const expected =
       {
