a1db318403e0a79bf37ca9fd90ec466077589c23
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-4.local>
2018-03-28 09:11:09 -0400

    add subscription model

---
+++ addon/models/subscription.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+import OsfModel from './osf-model';
+
+/**
+ * @module ember-osf
+ * @submodule models
+ */
+
+/**
+ * Model for OSF APIv2 subscriptions.
+ * @class Subscription
+ */
+export default OsfModel.extend({
+    eventName: DS.attr('string'),
+    frequency: DS.attr('string'),
+});
