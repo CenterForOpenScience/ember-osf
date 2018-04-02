1e29f7faa9dfb6fe73651eca20a5e4d6353722ac
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-4.local>
2018-04-02 10:01:55 -0400

    fix linting

---
+++ tests/unit/models/subscription-test.js
@@ -0,0 +1,27 @@
+import { moduleForModel, test } from 'ember-qunit';
+
+moduleForModel('subscription', 'Unit | Model | contributor', {
+    // Specify the other units that are required for this test.
+    needs: [
+        'transform:links',
+        'transform:embed',
+    ]
+});
+
+test('it exists', function(assert) {
+    let model = this.subject();
+    // let store = this.store();
+    assert.ok(!!model);
+});
+
+test('it has an attribute: frequency', function(assert) {
+    var model = this.subject();
+    var hasAttr = Object.keys(model.toJSON()).indexOf('frequency') > -1;
+    assert.ok(hasAttr);
+});
+
+test('it has an attribute: eventName', function(assert) {
+    var model = this.subject();
+    var hasAttr = Object.keys(model.toJSON()).indexOf('eventName') > -1;
+    assert.ok(hasAttr);
+});
