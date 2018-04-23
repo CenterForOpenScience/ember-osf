8561edd286a954f3729ca4321e63ec94ba8ec9db
Lauren Barker <lauren@cos.io>
2018-04-23 10:43:32 -0400

    Use template strings

--- addon/adapters/moderator.js
+++ addon/adapters/moderator.js
@@ -12,15 +12,15 @@ export default OsfAdapter.extend({
         } else if (requestType === 'createRecord' && snapshot.record.provider) {
             provider = snapshot.record.provider;
             delete snapshot.record.provider;
-            return this._buildURL(modelName) + `/${provider}/moderators/`;
+            return `${this._buildURL(modelName)}/${provider}/moderators/`;
         } else if (snapshot.adapterOptions && snapshot.adapterOptions.provider) {
             provider = snapshot.adapterOptions.provider;
             delete snapshot.adapterOptions.provider;
             if (requestType === 'updateRecord' || requestType === 'deleteRecord' || requestType === 'findRecord') {
-                return this._buildURL(modelName) + `/${provider}/moderators/${id}/`;
+                return `${this._buildURL(modelName)}/${provider}/moderators/${id}/`;
             }
         }
 
-        return this._super(...arguments) + `${provider}/moderators/`;
+        return `${this._super(...arguments)}${provider}/moderators/`;
     }
 });
