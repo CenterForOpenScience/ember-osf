639774da35c82ddcb8466d47fd181a578a84d283
Yuhuai Liu <yuhuailiu@Admins-MacBook-Pro-4.local>
2018-05-01 11:16:16 -0400

    remove redundant line

--- tests/unit/models/subscription-test.js
+++ tests/unit/models/subscription-test.js
@@ -10,7 +10,6 @@ moduleForModel('subscription', 'Unit | Model | contributor', {
 
 test('it exists', function(assert) {
     let model = this.subject();
-    // let store = this.store();
     assert.ok(!!model);
 });
 
