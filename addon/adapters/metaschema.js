cb1b0d10aeed474569670c760616f908efaa966c
Baylee Swenson <baylee@cos.io>
2018-06-01 16:25:57 -0400

    Add params to metaschema adapter

--- addon/adapters/metaschema.js
+++ addon/adapters/metaschema.js
@@ -1,3 +1,12 @@
 import OsfAdapter from './osf-adapter';
 
-export default OsfAdapter.extend({});
+export default OsfAdapter.extend({
+    buildURL(modelName, id, snapshot, requestType, query) {
+        let metaschemaType = '';
+        if (query) {
+            metaschemaType = query.metaschemaType;
+            delete query.metaschemaType;
+        }
+        return `${this._super(...arguments)}${metaschemaType}/`;
+    },
+});
