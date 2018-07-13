f3d87612d701b25f1cc405dca3f945f932d515ef
Yuhuai Liu <yuhuai@cos.io>
2018-07-13 10:52:32 -0400

    add preprint-request model, override buildURL for its adapter

---
+++ addon/adapters/preprint-request.js
@@ -0,0 +1,20 @@
+import OsfAdapter from './osf-adapter';
+import config from 'ember-get-config';
+
+export default OsfAdapter.extend({
+    buildURL: function(modelName, id, snapshot, requestType, query) {
+        let url;
+        if (requestType === 'query') {
+            const providerId = query.providerId;
+            const preprintId = query.preprintId;
+            if (preprintId) {
+                url = `${this.host}/${config.OSF.apiNamespace}/providers/preprints/${providerId}/withdraw_requests/?filter[target]=${preprintId}`;
+                delete query.preprintId;
+            } else {
+                url = `${this.host}/${config.OSF.apiNamespace}/providers/preprints/${query.providerId}/withdraw_requests/`;
+                delete query.providerId;
+            }
+        }
+        return url;
+    },
+});
