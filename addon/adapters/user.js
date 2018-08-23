6137b1c799ca97ce716e9aeb6ce8fb2d92573ca2
Alex Schiller <alexschiller@gmail.com>
2018-08-23 09:29:50 -0400

    Modify user query if 'q' present

--- addon/adapters/user.js
+++ addon/adapters/user.js
@@ -1,4 +1,12 @@
 import OsfAdapter from './osf-adapter';
 
 export default OsfAdapter.extend({
+    urlForQuery(query, modelName) {
+        // if query contains `q` for search, use the elasticsearch `/search/users/` endpoint
+        if(query.hasOwnProperty('q')) {
+            const originalPathForTypeRegEx = new RegExp(`${this.pathForType(modelName)}$`);
+            return this._super(query, modelName).replace(originalPathForTypeRegEx, 'search/users');
+        }
+        return this._super(query, modelName);
+    }
 });
