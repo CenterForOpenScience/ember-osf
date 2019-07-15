3e37e2639af60420b621a259bac54aa294725def
Yuhuai Liu <yuhuai@cos.io>
2019-07-15 11:36:20 -0400

    change chronos-submission adapter

--- addon/adapters/chronos-submission.js
+++ addon/adapters/chronos-submission.js
@@ -12,5 +12,21 @@ export default OsfAdapter.extend({
         const preprintId = snapshot.belongsTo('preprint').id;
         const url = `${this.host}/${this.namespace}/${preprintId}/submissions/`;
         return url;
+    },
+
+    urlForUpdateRecord: function (id, modelName, snapshot) {
+        const preprintId = snapshot.belongsTo('preprint').id;
+        const submissionId = snapshot.id;
+        const url = `${this.host}/${this.namespace}/${preprintId}/submissions/${submissionId}`;
+        return url;
+    },
+
+    updateRecord: function (store, type, snapshot) {
+        let data = {};
+        let serializer = store.serializerFor(type.modelName);
+        serializer.serializeIntoHash(data, type, snapshot);
+        let id = snapshot.id;
+        let url = this.buildURL(type.modelName, id, snapshot, 'updateRecord');
+        return this.ajax(url, "PUT", { data });
     }
 });
